85f03ac39cd85c887b5a69ce1d3c143939754d36
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-09 18:03:15 +0500

    User model added

---
+++ server/src/models/user.js
@@ -0,0 +1,91 @@
+const { Schema, model } = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+const bcrypt = require("bcrypt");
+
+// Schema
+const userSchema = new Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    email:{
+        type:String,
+        trim:true,
+        lowercase:true,
+        unique:true,
+        required:true,       
+    },
+    username:{
+        type:String,
+        trim:true,
+        lowercase:true,
+        unique:true,
+        index:true,
+        required:true
+    },
+    password:{
+        type:String,
+        trim:true,
+        required:true
+    },  
+    role:{
+        type:String,
+        enum:["Admin", "User"],
+        default:"User",
+        trim:true,
+        required:true
+    },
+}, { timestamps:true });
+
+// Inject pagination plugin
+userSchema.plugin(mongoosePaginate);
+
+// Hash password before save
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+    try 
+    {
+        this.password = await bcrypt.hash(this.password, 10);
+        return next();
+    } 
+    catch (error) 
+    {
+        console.log(error.message);
+        return next();
+    }
+});
+
+// Match password
+userSchema.methods.matchPassword = async function(password) {
+    if(!password) return false;
+    try 
+    {
+       return await bcrypt.compare(password, this.password); 
+    } 
+    catch (error) 
+    {
+        console.log(error.message);
+        return false;
+    }
+}
+
+// Get user by email
+userSchema.static("getUserByEmail", async function(email) {
+    try 
+    {
+        const user = await this.findOne({ email });
+        if(!user) return null;
+        return user;
+    } 
+    catch (error) 
+    {
+        console.log(error.message);
+        return null;
+    }
+});
+
+// Model
+const User = model("User", userSchema);
+
+module.exports = User;
