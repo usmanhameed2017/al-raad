410c8c522defda3b9d22e56b09055a337cc77338
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-09 17:34:34 +0500

    Wrapper for error handling

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const cookieParse = require("cookie-parser");
 const path = require("path");
 const { corsOptions } = require("./config");
+const errorHandler = require("./middlewares/errorHandler");
 
 // Express app
 const app = express();
@@ -13,5 +14,8 @@ app.use(express.urlencoded({ extended:true, limit:"20kb" }));
 app.use(express.json({ limit:"20kb" }));
 app.use(cookieParse());
 app.use("/public", express.static(path.resolve("public")));
+
+// Error handling middleware
+app.use(errorHandler);
 
 module.exports = app;
