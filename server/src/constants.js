6cb774a7a944a60d9a4564c130ada4251d86431b
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-09 16:39:03 +0500

    Added db credentials

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,4 +1,28 @@
+// Ports and origins
 const port = process.env.PORT || 8000;
 const origin = process.env.ORIGIN;
 
-module.exports = { port, origin };
+// Database
+const mongoURL = process.env.MONGO_URL;
+const dbName = process.env.DB_NAME;
+
+// JWT
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+const accessTokenExpiry = process.env.ACCESS_TOKEN_EXPIRY;
+
+// Cloudinary
+const cloudinary_cloud_name = process.env.CLOUDINARY_CLOUD_NAME;
+const cloudinary_api_key = process.env.CLOUDINARY_API_KEY
+const cloudinary_api_secret = process.env.CLOUDINARY_API_SECRET
+
+module.exports = { 
+    port, 
+    origin, 
+    mongoURL, 
+    dbName, 
+    accessTokenSecret, 
+    accessTokenExpiry,
+    cloudinary_cloud_name,
+    cloudinary_api_key,
+    cloudinary_api_secret
+};
