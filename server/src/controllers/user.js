90e1585d20671b53f09bd51f2daeb8e31a15312d
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-10 16:36:15 +0500

    Rename from verificationCode to activationCode

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -3,11 +3,13 @@ const sendEmail = require("../service/mailer");
 const ApiError = require("../utils/ApiError");
 const ApiResponse = require("../utils/ApiResponse");
 const shortid = require("shortid");
+const fs = require("fs");
+const path = require("path");
 
 // User signup
 const signup = async (request, response) => {
-    const { email, username, password, cpassword } = request.body;
-    if([email, username, password, cpassword].some(field => field === "")) throw new ApiError(400, "All fields are required");
+    const { name, email, username, password, cpassword } = request.body;
+    if([name, email, username, password, cpassword].some(field => field === "")) throw new ApiError(400, "All fields are required");
 
     if(password !== cpassword) throw new ApiError(400, "Password & confirm password must be identical");
 
@@ -20,13 +22,13 @@ const signup = async (request, response) => {
         if(user.status === "Pending")
         {
             // Generate verification code
-            const verificationCode = shortid.generate();
+            const activationCode = shortid.generate();
 
             // Update verification code
-            const updateUser = await User.findByIdAndUpdate(user?._id, { verificationCode }, { new:true }).select("-password");
+            const updateUser = await User.findByIdAndUpdate(user?._id, { activationCode }, { new:true }).select("-password");
 
             // Send email
-            const result = sendEmail(email, "Account Activation", `Your account activation code is ${verificationCode}`);
+            const result = sendEmail(email, "Account Activation", `Your account activation code is ${activationCode}`);
             if(!result) throw new ApiError(500, "Unable to send email"); 
 
             return response.status(200)
@@ -37,16 +39,27 @@ const signup = async (request, response) => {
     try 
     {
         // Generate verification code
-        const verificationCode = shortid.generate();
-        request.body.verificationCode = verificationCode;
+        const activationCode = shortid.generate();
+        request.body.activationCode = activationCode;
 
         // Create user
         const createUser = await User.create(request.body);
         const userData = createUser.toObject();
         delete userData.password; // Exclude password
 
+        // Get HTML template
+        const html = fs.readFileSync(path.resolve(__dirname, "../../public/accountActivation.html"), "utf-8");
+
+        // Replace placeholders
+        const filledHtml = html
+        .replace('{{name}}', name)
+        .replace('{{activationCode}}', activationCode);
+
+        // Send mail
+        const result = await sendEmail(email, "Account Activation", filledHtml);      
+
         // Send email
-        const result = sendEmail(email, "Account Activation", `Your account activation code is ${verificationCode}`);
+        // const result = sendEmail(email, "Account Activation", `Your account activation code is ${activationCode}`);
         if(!result) throw new ApiError(500, "Unable to send email");
 
         return response.status(201)
@@ -60,12 +73,12 @@ const signup = async (request, response) => {
 
 // Account activation
 const accountActivation = async (request, response) => {
-    const { verificationCode } = request.body;
-    if(verificationCode.trim() === "") throw new ApiError(400, "Activation code is required");
+    const { activationCode } = request.body;
+    if(activationCode.trim() === "") throw new ApiError(400, "Activation code is required");
 
     try 
     {
-       const user = await User.findOneAndUpdate({ verificationCode }, { status:"Approved" }, { new:true }).select("-password");
+       const user = await User.findOneAndUpdate({ activationCode }, { status:"Approved" }, { new:true }).select("-password");
        if(!user) throw new ApiError(404, "Invalid activation code");
 
        return response.status(200).json(new ApiResponse(200, user, "Your account has been activated successfully!"));
