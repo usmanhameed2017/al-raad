1dc158087a896a771378854f9da6a87271a5f2c7
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-10 20:45:06 +0500

    Added route to delete  user

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,4 +1,4 @@
-const { signup, accountActivation, login, logout, fetchUsers, editUser } = require("../controllers/user");
+const { signup, accountActivation, login, logout, fetchUsers, editUser, deleteUser } = require("../controllers/user");
 const { authentication, authorization } = require("../middlewares/auth");
 
 const userRouter = require("express").Router();
@@ -12,10 +12,11 @@ userRouter.route("/accountActivation").put(accountActivation);
 // Login
 userRouter.route("/login").get(login);
 
-// Fetch users
+// Multi operations
 userRouter.route("/")
-.get(authentication, authorization(["Admin"]), fetchUsers) // Fetch all user
-.put(authentication, authorization(["Admin"]), editUser);  // Edit user
+.get(authentication, authorization(["Admin"]), fetchUsers)  // Fetch all user
+.put(authentication, authorization(["Admin"]), editUser)    // Edit user
+.put(authentication, authorization(["Admin"]), deleteUser); // Delete user
 
 // Logout
 userRouter.route("/logout").get(authentication, logout);
