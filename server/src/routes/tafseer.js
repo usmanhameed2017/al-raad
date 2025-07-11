1baad0ed875999fe530c39173446324cdd3744c1
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-11 19:13:57 +0500

    Added route for fetching all tafseers

--- server/src/routes/tafseer.js
+++ server/src/routes/tafseer.js
@@ -1,10 +1,11 @@
-const { createTafseer } = require("../controllers/tafseer");
+const { createTafseer, fetchTafseers } = require("../controllers/tafseer");
 const { authentication, authorization } = require("../middlewares/auth");
 
 const tafseerRouter = require("express").Router();
 
 // Create tafseer
 tafseerRouter.route("/")
-.post(authentication, authorization(["Admin"]), createTafseer);
+.post(authentication, authorization(["Admin"]), createTafseer)
+.get(authentication, authorization(["Admin"]), fetchTafseers);
 
 module.exports = tafseerRouter;
