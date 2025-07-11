6af4530732ce16e00a22f971fe225f9e841ac94c
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-11 19:48:00 +0500

    Added route for updating tafseer

--- server/src/routes/tafseer.js
+++ server/src/routes/tafseer.js
@@ -1,4 +1,4 @@
-const { createTafseer, fetchTafseers, fetchSingleTafseer } = require("../controllers/tafseer");
+const { createTafseer, fetchTafseers, fetchSingleTafseer, updateTafseer } = require("../controllers/tafseer");
 const { authentication, authorization } = require("../middlewares/auth");
 
 const tafseerRouter = require("express").Router();
@@ -9,5 +9,6 @@ tafseerRouter.route("/")
 
 tafseerRouter.route("/:id")
 .get(authentication, authorization(["Admin"]), fetchSingleTafseer)
+.put(authentication, authorization(["Admin"]), updateTafseer)
 
 module.exports = tafseerRouter;
