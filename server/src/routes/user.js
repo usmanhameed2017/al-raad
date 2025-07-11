c6065c1b4471f5bbfc166f9d82a24d8e9b23817d
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-11 18:03:06 +0500

    Added route for fetching single user

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,4 +1,4 @@
-const { signup, accountActivation, login, logout, fetchUsers, editUser, deleteUser } = require("../controllers/user");
+const { signup, accountActivation, login, logout, fetchUsers, editUser, deleteUser, fetchSingleUser } = require("../controllers/user");
 const { authentication, authorization } = require("../middlewares/auth");
 
 const userRouter = require("express").Router();
@@ -12,9 +12,12 @@ userRouter.route("/accountActivation").put(accountActivation);
 // Login
 userRouter.route("/login").get(login);
 
+// Fetch all user
+userRouter.route("/").get(authentication, authorization(["Admin"]), fetchUsers)
+
 // Multi operations
-userRouter.route("/")
-.get(authentication, authorization(["Admin"]), fetchUsers)  // Fetch all user
+userRouter.route("/:id")
+.get(authentication, authorization(["Admin"]), fetchSingleUser)  // Fetch single user
 .put(authentication, authorization(["Admin"]), editUser)    // Edit user
 .put(authentication, authorization(["Admin"]), deleteUser); // Delete user
 
