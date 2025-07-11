e602c59adce1c9df8278c53b40e87aed372c27dc
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-11 18:04:44 +0500

    Added route for creating tafseer

--- server/src/routes/tafseer.js
+++ server/src/routes/tafseer.js
@@ -1,5 +1,10 @@
+const { createTafseer } = require("../controllers/tafseer");
 const { authentication, authorization } = require("../middlewares/auth");
 
 const tafseerRouter = require("express").Router();
+
+// Create tafseer
+tafseerRouter.route("/")
+.post(authentication, authorization(["Admin"]), createTafseer);
 
 module.exports = tafseerRouter;
