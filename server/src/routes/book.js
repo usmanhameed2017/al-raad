5afe008fc8214a6b8f48ce44dc39987f2bf543e4
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-14 20:42:09 +0500

    Route added for fetching single book

--- server/src/routes/book.js
+++ server/src/routes/book.js
@@ -1,12 +1,15 @@
-const { createBook, fetchBooks } = require("../controllers/book");
+const { createBook, fetchBooks, fetchSingleBook } = require("../controllers/book");
 const { authentication, authorization } = require("../middlewares/auth");
 const upload = require("../middlewares/multer");
 
 const bookRouter = require("express").Router();
 
-// Create book
+
 bookRouter.route("/")
-.post(authentication, authorization(["Admin"]), upload.fields([{ name:"pdf", maxCount:1 }, { name:"coverImage", maxCount:1 }]), createBook)
-.get(fetchBooks);
+.post(authentication, authorization(["Admin"]), upload.fields([{ name:"pdf", maxCount:1 }, { name:"coverImage", maxCount:1 }]), createBook) // Create book
+.get(fetchBooks); // Fetch all books
+
+bookRouter.route("/:id")
+.get(fetchSingleBook)
 
 module.exports = bookRouter;
