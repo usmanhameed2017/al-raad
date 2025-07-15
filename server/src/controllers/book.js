d972fe98b430739bd536ed34b5ca19274205e5d4
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-15 21:08:57 +0500

    Controller added for updating a book

--- server/src/controllers/book.js
+++ server/src/controllers/book.js
@@ -2,7 +2,7 @@ const Book = require("../models/book");
 const ApiError = require("../utils/ApiError");
 const ApiResponse = require("../utils/ApiResponse");
 const { isValidObjectId } = require("mongoose");
-const { deleteFromTemp, uploadOnCloudinary } = require("../utils/cloudinary");
+const { deleteFromTemp, uploadOnCloudinary, deleteFromCloudinary } = require("../utils/cloudinary");
 
 // Create book
 const createBook = async (request, response) => {
@@ -84,4 +84,74 @@ const fetchSingleBook = async (request, response) => {
     }
 };
 
-module.exports = { createBook, fetchBooks, fetchSingleBook };
+// Edit book
+const editBook = async (request, response) => {
+    // Get uploaded file's url
+    const uploadedPdf = request.files?.pdf?.[0]?.path || "";
+    const uploadedCoverImage = request.files?.coverImage?.[0]?.path || "";
+
+    // Validate id
+    const id = request.params?.id;
+    if(!id) 
+    {
+        deleteFromTemp(uploadedPdf, uploadedCoverImage);
+        throw new ApiError(404, "Book ID is missing");
+    }
+
+    if(!isValidObjectId(id)) 
+    {
+        deleteFromTemp(uploadedPdf, uploadedCoverImage);
+        throw new ApiError(400, "Invalid MongoDB ID");
+    }
+
+    try 
+    {
+        
+        const book = await Book.findById(id);
+        if(!book)
+        {
+            deleteFromTemp(uploadedPdf, uploadedCoverImage);
+            throw new ApiError(404, "Book not found");     
+        }
+
+        // Get old file's url
+        const oldPdf = book?.pdf || "";
+        const oldCoverImage = book?.coverImage || "";
+
+        // If new files uploaded
+        if(uploadedPdf) 
+        {
+            const pdfUrl = await uploadOnCloudinary(uploadedPdf, "raw", "pdf");
+            request.body.pdf = pdfUrl?.replace("/upload/", "/upload/fl_attachment/"); // Download pdf forcefully
+
+            const updatedPdfUrl = oldPdf.replace("/upload/fl_attachment/", "/upload/");
+            await deleteFromCloudinary(updatedPdfUrl, "raw", "pdf"); // Delete from cloudinary
+        }
+        else
+        {
+            request.body.pdf = oldPdf;
+        }
+
+        if(uploadedCoverImage)
+        {
+            request.body.coverImage = await uploadOnCloudinary(uploadedCoverImage, "image", "images");
+            await deleteFromCloudinary(oldCoverImage, "image", "images"); // Delete from cloudinary
+        }
+        else
+        {
+            request.body.coverImage = oldCoverImage;
+        }
+
+        // Update book
+        const updateBook = await Book.findByIdAndUpdate(id, request.body, { new:true });
+        return response.status(200).json(new ApiResponse(200, updateBook, "Book has been updated"));
+    } 
+    catch (error) 
+    {
+        deleteFromTemp(uploadedPdf, uploadedCoverImage);
+        throw new ApiError(404, error.message);
+    }
+
+};
+
+module.exports = { createBook, fetchBooks, fetchSingleBook, editBook };
