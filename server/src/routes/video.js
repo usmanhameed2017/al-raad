eb7e57eb4ffc273e323991187879fbbe9cdf6fb2
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-16 16:13:59 +0500

    Route added for uploading a video

--- server/src/routes/video.js
+++ server/src/routes/video.js
@@ -1,8 +1,12 @@
+const { createVideo } = require("../controllers/video");
 const { authentication, authorization } = require("../middlewares/auth");
 const upload = require("../middlewares/multer");
 
 // Video router instance
 const videoRouter = require("express").Router();
 
+// Create video
+videoRouter.route("/")
+.post(authentication, authorization(["Admin"]), upload.single("url"), createVideo);
 
 module.exports = videoRouter;
