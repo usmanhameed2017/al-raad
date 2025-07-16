7347c4ee007695f5668031dbebd5206e36502e08
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-16 17:19:16 +0500

    Controller added for updating a video

--- server/src/controllers/video.js
+++ server/src/controllers/video.js
@@ -64,12 +64,14 @@ const fetchVideos = async (request, response) => {
 
 // Fetch single video
 const fetchSingleVideo = async (request, response) => {
+    // Validate id
     const id = request.params?.id;
     if(!id) throw new ApiError(404, "Video ID is missing");
     if(!isValidObjectId(id)) throw new ApiError(400, "Invalid MongoDB ID");
 
     try 
     {
+        // Find video
         const video = await Video.findById(id);
         if(!video) throw new ApiError(404, "Video not found");
         return response.status(200).json(new ApiResponse(200, video, "Video has been fetched successfully"));
@@ -80,4 +82,56 @@ const fetchSingleVideo = async (request, response) => {
     }
 };
 
-module.exports = { createVideo, fetchVideos, fetchSingleVideo };
+// Update video
+const updateVideo = async (request, response) => {
+    const uploadedVideo = request.file?.path || "";
+
+    // Validate id
+    const id = request.params?.id;
+    if(!id) 
+    {
+        deleteFromTemp(uploadedVideo);
+        throw new ApiError(404, "Video ID is missing");
+    }
+    if(!isValidObjectId(id)) 
+    {
+        deleteFromTemp(uploadedVideo);
+        throw new ApiError(400, "Invalid MongoDB ID");
+    }
+
+    try 
+    {
+        // Find video
+        const video = await Video.findById(id);
+        if(!video)
+        {
+            deleteFromTemp(uploadedVideo);
+            throw new ApiError(404, "Video not found");
+        }
+
+        // Get old video url
+        const oldVideo = video?.url || "";
+
+        // If new video uploaded
+        if(uploadedVideo)
+        {
+            request.body.url = await uploadOnCloudinary(uploadedVideo, "video", "videos");
+            await deleteFromCloudinary(oldVideo, "video", "videos");
+        }
+        else
+        {
+            request.body.url = oldVideo;
+        }
+
+        // Update video
+        const updatedVideo = await Video.findByIdAndUpdate(id, request.body, { new:true });
+        return response.status(200).json(new ApiResponse(200, updatedVideo, "Video has been updated successfully"));
+    } 
+    catch (error) 
+    {
+        deleteFromTemp(uploadedVideo);
+        throw new ApiError(404, error.message);
+    }
+};
+
+module.exports = { createVideo, fetchVideos, fetchSingleVideo, updateVideo };
