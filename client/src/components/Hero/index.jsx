239cb6fdb25671a25eb5c734256a41c080f44932
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-16 19:03:40 +0500

    Hero component added

---
+++ client/src/components/Hero/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './style.module.css';
+import Button from '../Button';
+
+function Hero() 
+{
+    return (
+        <div className={styles.heroContainer}>
+
+            {/* Background video */}
+            <video 
+            className={styles.videoBackground}
+            src="/public/hero-section-video.mp4"
+            autoPlay
+            muted
+            loop
+            playsInline> </video>
+
+            {/* Content */}
+            <div className={styles.content}>
+                <h1> WELCOME TO AL-RA’AD </h1>
+                <p> Discover Tafseer, Books, and Daily Ayat </p>
+                <Button onClick={ () => alert("Ok") }> Get Started </Button>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
