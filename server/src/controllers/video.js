bb5ef93649dc74a6236d67ee67a3ee8db5fda0a1
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-16 16:23:51 +0500

    Controller added for fetching videos

--- server/src/controllers/video.js
+++ server/src/controllers/video.js
@@ -35,4 +35,49 @@ const createVideo = async (request, response) => {
     }
 };
 
-module.exports = { createVideo };
+// Fetch all videos
+const fetchVideos = async (request, response) => {
+    const { page = 1, limit = 10 } = request.query;
+
+    // Paging options
+    const options = {
+        page:parseInt(page),
+        limit:parseInt(limit),
+        sort: { createdAt: -1 },
+    };
+
+    try 
+    {
+        // Execute query
+        const result = await Video.paginate({}, options);
+
+        // If page size is greater than total pages
+        if(page > result.totalPages) throw new ApiError(404, "Video not found");
+
+        return response.status(200).json(new ApiResponse(200, result, "All videos has been fetched successfully"));
+    } 
+    catch(error) 
+    {
+        throw new ApiError(404, error.message);
+    }
+};
+
+// Fetch single video
+const fetchSingleVideo = async (request, response) => {
+    const id = request.params?.id;
+    if(!id) throw new ApiError(404, "Video ID is missing");
+    if(!isValidObjectId(id)) throw new ApiError(400, "Invalid MongoDB ID");
+
+    try 
+    {
+        const video = await Video.findById(id);
+        if(!video) throw new ApiError(404, "Video not found");
+        return response.status(200).json(new ApiResponse(200, video, "Video has been fetched successfully"));
+    } 
+    catch (error) 
+    {
+        throw new ApiError(404, error.message);
+    }
+};
+
+module.exports = { createVideo, fetchVideos, fetchSingleVideo };
