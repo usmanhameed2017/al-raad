91123bed11ce994af879aebe0667528a84a84beb
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-17 18:22:38 +0500

    Added icons on links

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import styles from "./style.module.css";
+import { Link } from "react-router-dom";
+import { FaUserAlt, FaHome, FaInfoCircle, FaEnvelopeOpenText, FaBookOpen, FaBook, FaVideo } from "react-icons/fa";
 
 function Navbar() 
 {
@@ -8,12 +10,13 @@ function Navbar()
         <nav className={styles.navbar}>
             <div className={styles.logo}> Al-Ra’ad </div>
             <div className={`${styles.navLinks} ${isOpen ? styles.show : ""}`}>
-                <a href="/">Home</a>
-                <a href="/tafseer">About</a>
-                <a href="/tafseer">Contact</a>
-                <a href="/tafseer">Tafseer</a>
-                <a href="/books">Books</a>
-                <a href="/videos">Videos</a>
+                <Link to="/"> <FaHome size={25} style={{ marginRight: "5px" }} /> Home </Link>
+                <Link to="/about"> <FaInfoCircle size={25} style={{ marginRight: "5px" }} /> About </Link>
+                <Link to="/contact"> <FaEnvelopeOpenText size={25} style={{ marginRight: "5px" }} /> Contact </Link>
+                <Link to="/tafseer"> <FaBookOpen size={25} style={{ marginRight: "5px" }} /> Tafseer </Link>
+                <Link to="/books"> <FaBook size={25} style={{ marginRight: "5px" }} /> Books </Link>
+                <Link to="/videos"> <FaVideo size={25} style={{ marginRight: "5px" }} /> Videos </Link>
+                <Link to="/login"> <FaUserAlt style={{ marginRight: "5px" }} /> Signin/Signup </Link>
             </div>
             <div className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
                 <span></span><span></span><span></span>
