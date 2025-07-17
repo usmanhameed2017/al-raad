9be251e7e127bbde858e3c9376fbb05be2f5e27f
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-17 17:30:15 +0500

    Routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,31 @@
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/marketing/Home";
+import Tafseer from "./pages/marketing/Tafseer";
+import MarketingLayout from "./layout/MarketingLayout";
+import AdminLayout from "./layout/AdminLayout";
+import Dashboard from "./pages/admin/Dashboard";
+import Settings from "./pages/admin/Settings";
 
 function App() 
 {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-    </div>
+    <>
+      <Routes>
+        {/* Marketing Layout */}
+        <Route path="/" element={ <MarketingLayout /> } >
+          <Route index element={ <Home /> } />
+          <Route path="home" element={ <Home /> } />
+          <Route path="tafseer" element={ <Tafseer /> } />
+        </Route>
+
+        {/* Admin Layout */}
+        <Route path="/admin" element={ <AdminLayout /> } >
+          <Route index element={ <Dashboard /> } />
+          <Route path="dashboard" element={ <Dashboard /> } />
+          <Route path="settings" element={ <Settings /> } />
+        </Route>        
+      </Routes>
+    </>
   );
 }
 
