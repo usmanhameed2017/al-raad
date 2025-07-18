8da2df7be287b0e4b1f17de2c4b45749944e7079
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-18 17:09:07 +0500

    Auth provider added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,30 +9,34 @@ import About from "./pages/marketing/About";
 import Books from "./pages/marketing/Books";
 import Videos from "./pages/marketing/Videos";
 import Login from "./pages/marketing/Login";
+import AuthProvider from "./context/auth";
 
 function App() 
 {
   return (
     <>
-      <Routes>
-        {/* Marketing Layout */}
-        <Route path="/" element={ <MarketingLayout /> } >
-          <Route index element={ <Home /> } />
-          <Route path="home" element={ <Home /> } />
-          <Route path="about" element={ <About /> } />
-          <Route path="tafseer" element={ <Tafseer /> } />
-          <Route path="books" element={ <Books /> } />
-          <Route path="videos" element={ <Videos /> } />
-          <Route path="login" element={ <Login /> } />
-        </Route>
+      <AuthProvider>
+        <Routes>
 
-        {/* Admin Layout */}
-        <Route path="/admin" element={ <AdminLayout /> } >
-          <Route index element={ <Dashboard /> } />
-          <Route path="dashboard" element={ <Dashboard /> } />
-          <Route path="settings" element={ <Settings /> } />
-        </Route>        
-      </Routes>
+          {/* Marketing Layout */}
+          <Route path="/" element={ <MarketingLayout /> } >
+            <Route index element={ <Home /> } />
+            <Route path="home" element={ <Home /> } />
+            <Route path="about" element={ <About /> } />
+            <Route path="tafseer" element={ <Tafseer /> } />
+            <Route path="books" element={ <Books /> } />
+            <Route path="videos" element={ <Videos /> } />
+            <Route path="login" element={ <Login /> } />
+          </Route>
+
+          {/* Admin Layout */}
+          <Route path="/admin" element={ <AdminLayout /> } >
+            <Route index element={ <Dashboard /> } />
+            <Route path="dashboard" element={ <Dashboard /> } />
+            <Route path="settings" element={ <Settings /> } />
+          </Route>        
+        </Routes>
+      </AuthProvider>
     </>
   );
 }
---
+++ client/src/context/auth.js
@@ -0,0 +1,106 @@
+import axios from 'axios';
+import { useState, useEffect, createContext, useContext, useCallback } from 'react';
+import { axiosOptions, backendURL } from '../constants';
+import { ApiResponse } from '../utils/ApiResponse';
+import { ApiError } from '../utils/ApiError';
+import { useNavigate } from 'react-router-dom'
+import { showError, showSuccess } from '../utils/toasterMessage';
+
+const AuthContext = createContext();
+
+function AuthProvider({ children })
+{
+    const [isLoading, setLoading] = useState(false); // Loader
+    const [user, setUser] = useState(null); // User payload
+    const [isLoggedIn, setLoggedIn] = useState(null); // Login flag
+
+    const navigate = useNavigate();
+
+    // Signup
+    const userSignup = useCallback(async (user, action) => {
+        try 
+        {
+            setLoading(true);
+            const response = await axios.post(`${backendURL}/user/signup`, user);
+            action.resetForm();
+            setLoading(false);
+            showSuccess(ApiResponse(response).message);
+        } 
+        catch(error) 
+        {
+            setLoading(false);
+            showError(ApiError(error).message);
+        }
+    },[]);    
+
+    // Login
+    const userLogin = useCallback(async (user, action) => {
+        try 
+        {
+            setLoading(true);
+            const response = await axios.post(`${backendURL}/user/login`, user);
+            const { data, message, success } = ApiResponse(response);
+            setUser(data);
+            setLoggedIn(success);
+            setLoading(false);
+            localStorage.setItem("user", JSON.stringify(data));
+
+            action.resetForm();
+            showSuccess(message);
+            navigate('/');
+        } 
+        catch (error) 
+        {
+            setLoading(false);
+            showError(ApiError(error).message);
+        }
+    },[]);
+
+    // Logout
+    const userLogout = useCallback(async () => {
+        try 
+        {
+            await axios.get(`${backendURL}/user/logout`, axiosOptions);
+            setUser(null);
+            setLoggedIn(false);
+            localStorage.removeItem("user");
+            navigate("/");
+        } 
+        catch(error) 
+        {
+            showError(ApiError(error).message);
+        }
+    },[]);
+
+    // Verify Access Token
+    const verifyAccessToken = useCallback(async () => {
+        try 
+        {
+            const response = await axios.get(`${backendURL}/user/verifyAccessToken`, axiosOptions);
+            const { data, success } = ApiResponse(response);
+            setUser(data); // Plain user object
+            setLoggedIn(success);
+            localStorage.setItem("user", JSON.stringify(data));
+        } 
+        catch (error) 
+        {
+            setUser(null);
+            setLoggedIn(false);
+            localStorage.removeItem("user");
+        }
+    },[]);
+
+    useEffect(() => {
+        verifyAccessToken();
+    },[]);
+
+    return(
+        <AuthContext.Provider value={{ userSignup, userLogin, userLogout, isLoading, setLoading, isLoggedIn, setLoggedIn, user, setUser }}>
+            { children }
+        </AuthContext.Provider>
+    );
+}
+
+export const useAuth = () => useContext(AuthContext);
+
+export default AuthProvider;
