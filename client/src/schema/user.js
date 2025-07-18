464621015dd16dc478fa790492f051a558a794ee
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-18 19:16:03 +0500

    User schema added

---
+++ client/src/schema/user.js
@@ -0,0 +1,11 @@
+export const loginInitialValues = {
+    username:'',
+    password:''
+};
+
+export const signupInitialValues = {
+    name:'',
+    email:'',
+    username:'',
+    password:'',
+};
