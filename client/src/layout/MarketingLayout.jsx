c7a755b38b2ebd45ac65b0f0e7ca297e52cefdbe
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-25 17:45:12 +0500

    Change navbar

--- client/src/layout/MarketingLayout.jsx
+++ client/src/layout/MarketingLayout.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Outlet } from "react-router-dom";
-import Navbar from '../components/Navbar';
+import NavbarBS from '../components/Navbar';
 import Footer from '../components/Footer';
 
 function MarketingLayout() 
 {
     return (
         <div>
-            <Navbar />
+            <NavbarBS />
             <Outlet />
             <Footer />
         </div>
