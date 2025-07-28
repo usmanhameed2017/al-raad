4e65197c010c854a360cf344bf7ebf01150c11dc
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-28 15:08:41 +0500

    Adjusting input and label colors

--- client/src/pages/marketing/Login/index.jsx
+++ client/src/pages/marketing/Login/index.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import FormBS from "../../../components/Form";
 import Button from "../../../components/Button";
-import { loginInitialValues } from "../../../schema/user";
-import { loginValidation } from "../../../validation/user";
+import { loginInitialValues, signupInitialValues } from "../../../schema/user";
+import { loginValidation, signupValidation } from "../../../validation/user";
 import { useAuth } from "../../../context/auth";
 import styles from './style.module.css';
 
 function Login() 
 {
-    const { userLogin, isLoading } = useAuth();
+    const { userLogin, isLoading, userSignup } = useAuth();
     const formVariants = {
         hidden: { opacity: 0, y: 50 },
         visible: {
@@ -50,24 +50,25 @@ function Login()
                                 animate="visible"
                             >
                                 <Form>
+                                    {/* Username */}
                                     <div className="form-group">
                                         <label htmlFor="username">Username</label>
                                         <Field type='text' name='username' className='form-control' placeholder="Enter username" />
                                         <span className="text-danger"><ErrorMessage name="username" /></span>
                                     </div>
-
+                                    {/* Password */}
                                     <div className="form-group">
                                         <label htmlFor="password">Password</label>
                                         <Field type='password' name='password' className='form-control' placeholder="Enter password" />
                                         <span className="text-danger"><ErrorMessage name="password" /></span>
                                     </div>
-
+                                    {/* Forgot password */}
                                     <div className="form-group">
                                         <Link className="text-info" style={{ cursor: "pointer" }} to='/security/forgotPassword'>
                                             Forgot password?
                                         </Link>
                                     </div>
-
+                                    {/* Sign in */}
                                     <Button type="submit" className="custom-btn w-100 mt-3" disabled={ isLoading === false }>SIGN IN</Button>
                                     <hr />
                                 </Form>
@@ -77,82 +78,43 @@ function Login()
 
                     {/* Signup */}
                     <Tab eventKey="Signup" title="Signup">
-                        <FormBS>
-                            {({ setFieldValue }) => (
-                                <motion.div
-                                    variants={formVariants}
-                                    initial="hidden"
-                                    animate="visible"
-                                >
-                                    <Form>
-                                        <div className="form-group">
-                                            <label htmlFor="fname">First Name</label>
-                                            <Field type='text' name='fname' className='form-control' placeholder="Enter first name" />
-                                            <span className="text-danger"><ErrorMessage name="fname" /></span>
-                                        </div>
-
-                                        <div className="form-group">
-                                            <label htmlFor="lname">Last Name</label>
-                                            <Field type='text' name='lname' className='form-control' placeholder="Enter last name" />
-                                            <span className="text-danger"><ErrorMessage name="lname" /></span>
-                                        </div>
-
-                                        <div className="form-group">
-                                            <label htmlFor="age">Age</label>
-                                            <Field type='number' name='age' className='form-control' placeholder="Enter age" />
-                                            <span className="text-danger"><ErrorMessage name="age" /></span>
-                                        </div>
-
-                                        <div className="form-group">
-                                            <label htmlFor="gender">Gender</label>
-                                            <Field as="select" name='gender' className='form-control'>
-                                                <option value="">Select</option>
-                                                <option value="Male">Male</option>
-                                                <option value="Female">Female</option>
-                                            </Field>
-                                            <span className="text-danger"><ErrorMessage name="gender" /></span>
-                                        </div>
-
-                                        <div className="form-group">
-                                            <label htmlFor="email">Email</label>
-                                            <Field type='text' name='email' className='form-control' placeholder="Enter email" />
-                                            <span className="text-danger"><ErrorMessage name="email" /></span>
-                                        </div>
-
-                                        <div className="form-group">
-                                            <label htmlFor="username">Username</label>
-                                            <Field type='text' name='username' className='form-control' placeholder="Enter username" />
-                                            <span className="text-danger"><ErrorMessage name="username" /></span>
-                                        </div>
-
-                                        <div className="form-group">
-                                            <label htmlFor="password">Password</label>
-                                            <Field type='password' name='password' className='form-control' placeholder="Enter password" />
-                                            <span className="text-danger"><ErrorMessage name="password" /></span>
-                                        </div>
-
-                                        <div className="form-group">
-                                            <label htmlFor="cpassword">Confirm Password</label>
-                                            <Field type='password' name='cpassword' className='form-control' placeholder="Re-enter password" />
-                                            <span className="text-danger"><ErrorMessage name="cpassword" /></span>
-                                        </div>
-
-                                        <div className="form-group">
-                                            <label htmlFor="profile_image">Profile Image</label>
-                                            <input
-                                                type='file'
-                                                name='profile_image'
-                                                className='form-control'
-                                                accept="image/*"
-                                                onChange={(e) => setFieldValue("profile_image", e.target.files[0])}
-                                            />
-                                            <span className="text-danger"><ErrorMessage name="profile_image" /></span>
-                                        </div>
-
-                                        <Button type="submit" className="custom-btn w-100 mt-3">SIGN UP</Button>
-                                    </Form>
-                                </motion.div>
-                            )}
+                        <FormBS initialValues={signupInitialValues} validationSchema={signupValidation} handlerFunction={userSignup}>
+                            <motion.div variants={formVariants} initial="hidden" animate="visible">
+                                <Form>
+                                    {/* Name */}
+                                    <div className="form-group">
+                                        <label htmlFor="fname">Name</label>
+                                        <Field type='text' name='name' className='form-control' placeholder="Enter name" />
+                                        <span className="text-danger"><ErrorMessage name="name" /></span>
+                                    </div>
+                                    {/* Email */}
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <Field type='text' name='email' className='form-control' placeholder="Enter email" />
+                                        <span className="text-danger"><ErrorMessage name="email" /></span>
+                                    </div>
+                                    {/* Username */}
+                                    <div className="form-group">
+                                        <label htmlFor="username">Username</label>
+                                        <Field type='text' name='username' className='form-control' placeholder="Enter username" />
+                                        <span className="text-danger"><ErrorMessage name="username" /></span>
+                                    </div>
+                                    {/* Password */}
+                                    <div className="form-group">
+                                        <label htmlFor="password">Password</label>
+                                        <Field type='password' name='password' className='form-control' placeholder="Enter password" />
+                                        <span className="text-danger"><ErrorMessage name="password" /></span>
+                                    </div>
+                                    {/* Confirm password */}
+                                    <div className="form-group">
+                                        <label htmlFor="cpassword">Confirm Password</label>
+                                        <Field type='password' name='cpassword' className='form-control' placeholder="Re-enter password" />
+                                        <span className="text-danger"><ErrorMessage name="cpassword" /></span>
+                                    </div>
+                                    {/* Sign up */}
+                                    <Button type="submit" className="custom-btn w-100 mt-3">SIGN UP</Button>
+                                </Form>
+                            </motion.div>
                         </FormBS>
                     </Tab>
                 </Tabs>
