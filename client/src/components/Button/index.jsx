f68aa5bc7de33ee2f3a9ddf04260d8c37441bb96
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-28 19:42:30 +0500

    Updated button style

--- client/src/components/Button/index.jsx
+++ client/src/components/Button/index.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import style from './style.module.css';
 
 function Button({ children, type }) 
 {
     return (
-        <button className={style.animatedButton} type={type}>
+        <button className={style.themeBtn} type={type}>
             {children}
         </button>
     );
