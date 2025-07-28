d51a6481d40fe3d36126d8412b6e9db5804b334b
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-28 19:42:14 +0500

    Route added for contact page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import ProtectedRoute from "./pages/security/ProtectedRoutes";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AccountActivation from "./pages/marketing/Login/AccountActivation";
+import Contact from "./pages/marketing/Contact";
 
 function App() 
 {
@@ -27,6 +28,7 @@ function App()
             <Route index element={ <Home /> } />
             <Route path="home" element={ <Home /> } />
             <Route path="about" element={ <About /> } />
+            <Route path="contact" element={ <Contact /> } />
             <Route path="tafseer" element={ <Tafseer /> } />
             <Route path="books" element={ <Books /> } />
             <Route path="videos" element={ <Videos /> } />
