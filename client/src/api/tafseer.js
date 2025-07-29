0039095ac335ca0de8906f26d16e86bd1e0eb584
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-29 21:25:44 +0500

    Api integration

---
+++ client/src/api/tafseer.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { axiosOptions, backendURL } from '../constants';
+import { ApiResponse } from '../utils/ApiResponse';
+import { ApiError } from '../utils/ApiError';
+
+export const fetchTafseer = async (currentPage) => {
+    try 
+    {
+        const response = await axios.get(`${backendURL}/tafseer?page=${currentPage}`, axiosOptions);
+        return ApiResponse(response).data;
+    } 
+    catch(error) 
+    {
+        return ApiError(error);
+    }
+};
