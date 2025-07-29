121d920783fc5c31c5f29144ee72b36de049dbd3
Usman Hameed <usmanhameed1790@gmail.com>
2025-07-29 21:26:32 +0500

    Added onClick event on Button component

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -1,31 +1,33 @@
 import { Row, Col, Card } from 'react-bootstrap';
 import styles from './style.module.css';
 import Button from '../Button';
+import { useNavigate } from 'react-router-dom'
 
 
-function CardBS({ heading, subHeading, description }) 
+function CardBS({ _id, heading, subHeading, description }) 
 {
-  return (
-    <>
-        <Card className={`shadow ${styles.card}`}>
-            <Card.Header className={styles.cardHeader}>
-                { heading }
-            </Card.Header>
+    const navigate = useNavigate();
+    return (
+        <>
+            <Card className={`shadow ${styles.card}`}>
+                <Card.Header className={styles.cardHeader}>
+                    { heading }
+                </Card.Header>
 
-            <Card.Body className={styles.cardBody}>
-                <Card.Title className={styles.cardTitle}> { subHeading } </Card.Title>
-                <Card.Text className={styles.cardText}>
-                    { 
-                        description.length > 90 ? <> { description.substring(0, 90) }... </> : description
-                    }
-                </Card.Text>
-                <div className='d-grid'>
-                    <Button> View Tafseer</Button>
-                </div>
-            </Card.Body>
-        </Card>      
-    </>
-  )
+                <Card.Body className={styles.cardBody}>
+                    <Card.Title className={styles.cardTitle}> { subHeading } </Card.Title>
+                    <Card.Text className={styles.cardText}>
+                        { 
+                            description.length > 90 ? <> { description.substring(0, 90) }... </> : description
+                        }
+                    </Card.Text>
+                    <div className='d-grid'>
+                        <Button type="button" onClick={ () => navigate(`/tafseer/${_id}`) }> View Tafseer</Button>
+                    </div>
+                </Card.Body>
+            </Card>      
+        </>
+    );
 }
 
 export default CardBS;
