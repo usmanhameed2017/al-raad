7f37f88bb06793963b3027fabfb2ea24a3f195ba
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-01 15:03:18 +0500

    Added function for fetching single tafseer

--- client/src/api/tafseer.js
+++ client/src/api/tafseer.js
@@ -3,10 +3,24 @@ import { axiosOptions, backendURL } from '../constants';
 import { ApiResponse } from '../utils/ApiResponse';
 import { ApiError } from '../utils/ApiError';
 
+// Fetch all tafseers
 export const fetchAllTafseers = async (currentPage) => {
     try 
     {
         const response = await axios.get(`${backendURL}/tafseer?page=${currentPage}`, axiosOptions);
+        return ApiResponse(response).data;
+    } 
+    catch(error) 
+    {
+        return ApiError(error);
+    }
+};
+
+// Fetch single tafseer
+export const fetchSingleTafseer = async (id) => {
+    try 
+    {
+        const response = await axios.get(`${backendURL}/tafseer/${id}`, axiosOptions);
         return ApiResponse(response).data;
     } 
     catch(error) 
