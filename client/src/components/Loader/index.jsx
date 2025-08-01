2bf4230696dc660eb9e8736161b5479edeb237a5
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-01 15:01:23 +0500

    Created Loader component

---
+++ client/src/components/Loader/index.jsx
@@ -0,0 +1,13 @@
+import { FaSync } from 'react-icons/fa';
+import styles from './style.module.css';
+
+function Loading({ text }) 
+{
+    return (
+        <h3 className='text-center fw-bold textTheme'> 
+            <FaSync className={styles.spin} size={30} /> { text } 
+        </h3>
+    );
+}
+
+export default Loading;
