9e9d04105e767802f9b02924bb429e5bf5bb2296
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-01 16:03:08 +0500

    View tafseer page initial setup

--- client/src/pages/marketing/ViewTafseer/index.jsx
+++ client/src/pages/marketing/ViewTafseer/index.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchSingleTafseer } from '../../../api/tafseer';
 
 function ViewTafseer() 
 {
+    const { id } = useParams();
+    const [tafseer, setTafseer] = useState(null);
+
+    useEffect(() => {
+        fetchSingleTafseer(id)
+        .then(response => setTafseer(response))
+        .catch(error => setTafseer(error));
+    },[]);
+    console.log(tafseer);
+
     return (
         <div>
         
         </div>
     );
 }
+
 
 export default ViewTafseer;
