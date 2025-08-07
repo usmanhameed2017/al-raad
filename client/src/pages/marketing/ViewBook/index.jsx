8d0436969da70df752502b8e39ae3b171f01cc26
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-07 16:13:36 +0500

    View book page added

---
+++ client/src/pages/marketing/ViewBook/index.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchSingleBook } from '../../../api/book';
+import styles from './style.module.css';
+import { Row, Col } from 'react-bootstrap';
+import { useAuth } from '../../../context/auth';
+import { showError } from '../../../utils/toasterMessage';
+import Loader from '../../../components/Loader';
+import { getTime } from '../../../utils/getTime';
+import Animation from '../../../components/Animation';
+import Button from '../../../components/Button';
+
+function ViewBook() 
+{
+    const { id } = useParams();
+    const [book, setBook] = useState({});
+
+    const { isLoading, setLoading } = useAuth();
+
+    // Enable loader on page load
+    useEffect(() => {
+        setLoading(true);
+    },[]);
+
+    // Fetch book on page load
+    useEffect(() => {
+        fetchSingleBook(id)
+            .then(response => {
+                setLoading(false);
+                setBook(response);
+            })
+            .catch(error => {
+                setLoading(false);
+                showError(error);
+            });
+    }, [id]);
+
+    console.log(book);
+
+    return (
+        <div className={styles.bookContainer}>
+        {
+            isLoading === false ? 
+                book?._id ?
+                (
+                    <>
+                        {/* Surah & Ayat */}
+                        <Row>
+                            <Animation type="heading">
+                                <Col>
+                                    <h2 className={styles.bookName}> {book?.title} </h2>
+                                    <a href={book?.pdf} download>
+                                        <Button className='float-end'> Download </Button>
+                                    </a>
+                                    <hr />
+                                </Col>                                
+                            </Animation>
+                        </Row>
+
+                        {/* Description */}
+                        <Row className="mt-3">
+                            <Animation type="normal">
+                                <Col>
+                                    <p className={styles.bookDescription}> {book?.description} </p>
+                                </Col>                                
+                            </Animation>
+                        </Row> 
+
+                        {/* Uploaded Date */}
+                        <Row className='mt-5'>
+                            <Animation type="normal">
+                                <Col md={{ span:"3", offset:"9" }}> 
+                                    <p className='text-secondary'> Posted at: { getTime(book?.createdAt) } </p>
+                                </Col>                                
+                            </Animation>
+                        </Row>
+                    </>                    
+                )
+                :
+                (
+                    <Row className="mt-3">
+                        <Col>
+                            <h1 className='textTheme fw-bold text-center'> 404 - Book Not Found </h1>
+                        </Col>
+                    </Row>   
+                ) 
+            :
+            (
+                <>
+                    <Row style={{ marginTop:"200px" }}>
+                        <Col> <Loader text="Loading" /> </Col>
+                    </Row>
+                </>
+            )
+        }
+        </div>
+    );
+}
+
+export default ViewBook;
