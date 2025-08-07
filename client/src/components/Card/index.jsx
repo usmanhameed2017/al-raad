39d4e735c8fbbad8997700481aa773ad57a849ce
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-07 17:07:04 +0500

    Added card link to card component

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -1,10 +1,10 @@
-import { Row, Col, Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import styles from './style.module.css';
 import Button from '../Button';
 import { useNavigate } from 'react-router-dom'
 
 
-function CardBS({ _id, heading, subHeading, description, buttonText, redirectTo }) 
+function CardBS({ heading, subHeading, description, buttonText, redirectTo, link }) 
 {
     const navigate = useNavigate();
     return (
@@ -21,6 +21,9 @@ function CardBS({ _id, heading, subHeading, description, buttonText, redirectTo 
                             description.length > 90 ? <> { description.substring(0, 90) }... </> : description
                         }
                     </Card.Text>
+                    {
+                        link && ( <Card.Link href={link}> Download </Card.Link> )
+                    }
                     <Button type="button" onClick={ () => navigate(redirectTo) }> { buttonText } </Button>
                 </Card.Body>
             </Card>      
