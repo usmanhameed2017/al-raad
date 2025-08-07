9a1683cb30b776007d36a860987d6e3651924bd9
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-07 20:46:14 +0500

    Video card component added

---
+++ client/src/components/VideoCard/index.jsx
@@ -0,0 +1,28 @@
+import styles from './style.module.css';
+
+function VideoCard({ url, title }) 
+{
+    const thumbnail =  (videoUrl) => {
+        return videoUrl
+        .replace('/upload/', '/upload/so_2/')
+        .replace(/\.(mp4|webm|mov)$/i, '.jpg');
+    }
+
+    return (
+        <div className={styles.videoCard}>
+            <video 
+                className={styles.video}
+                controls
+                preload="none"
+                loading="lazy"
+                poster={thumbnail(url)}
+            >
+                <source src={url} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            { title && <div className={styles.caption}> {title} </div> }
+        </div>
+    );
+}
+
+export default VideoCard;
