eda7d0c9be8c00264a0522d6015f50fce7c29623
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-07 16:52:38 +0500

    Added scroll position for smooth page transition

--- client/src/layout/MarketingLayout/index.jsx
+++ client/src/layout/MarketingLayout/index.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from "react-router-dom";
 import NavbarBS from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import styles from './style.module.css';
+import { useLocation } from "react-router-dom";
 
 function MarketingLayout() 
 {
+    const { pathname } = useLocation();
+
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior:"smooth" });
+    }, [pathname]);
+
     return (
         <div className={styles.pageWrapper}>
             <NavbarBS />
