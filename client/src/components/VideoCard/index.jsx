3bd43788e3e53f5222419143cfc75b45f713a6f4
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-08 21:34:19 +0500

    Added function to get video thumbnail

--- client/src/components/VideoCard/index.jsx
+++ client/src/components/VideoCard/index.jsx
@@ -2,6 +2,7 @@ import styles from './style.module.css';
 
 function VideoCard({ url, title }) 
 {
+    // Function to extract thumbnail
     const thumbnail =  (videoUrl) => {
         return videoUrl
         .replace('/upload/', '/upload/so_2/')
@@ -10,18 +11,17 @@ function VideoCard({ url, title })
 
     return (
         <div className={styles.videoCard}>
-            <video 
-                className={styles.video}
-                controls
-                preload="none"
-                loading="lazy"
-                poster={thumbnail(url)}
-            >
+            <video className={styles.video} controls preload="none" loading="lazy" poster={thumbnail(url)}>
                 <source src={url} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             { title && <div className={styles.caption}> {title} </div> }
         </div>
+
+        // <audio controls preload="none">
+        //     <source src={url} type="audio/mp3" />
+        //     Your browser does not support the audio tag.
+        // </audio>
     );
 }
 
