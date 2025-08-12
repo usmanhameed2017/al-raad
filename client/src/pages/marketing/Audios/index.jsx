69b386bcb5aff24c6c600612f94b6a04430361bc
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-12 14:31:34 +0500

    Rename field name from title to surahName

--- client/src/pages/marketing/Audios/index.jsx
+++ client/src/pages/marketing/Audios/index.jsx
@@ -57,7 +57,7 @@ function Audios()
                 data.docs.map((audio, _) => (
                     <Col xs={12} sm={12} md={6} lg={4} xl={4} key={audio?._id} className='mb-3'>
                         <Animation type="normal">
-                            <AudioCard title={audio?.title} url={audio?.url} />
+                            <AudioCard title={audio?.surahName} description={audio?.ayah} url={audio?.url} />
                         </Animation>
                     </Col>                     
                 ))
