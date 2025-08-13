bc00341215d0f8afaa7b696d7dd6b1f002b9c4c2
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-13 21:40:44 +0500

    Added settings page

---
+++ client/src/pages/marketing/Settings/index.jsx
@@ -0,0 +1,77 @@
+import Animation from '../../../components/Animation';
+import Button from '../../../components/Button';
+import FormBS from '../../../components/Form';
+import { getUser } from '../../../constants';
+import { userSettingsValidation } from '../../../validation/user';
+import styles from './style.module.css';
+import { Form, Field, ErrorMessage } from 'formik';
+
+function UserSettings() 
+{
+    // Extract user properties
+    const user = getUser();
+
+    // Form initial values
+    const initialValue = {
+        _id: user?._id || "",
+        name: user?.name || "",
+        username: user?.username || "",
+        password:"",
+        cpassword:""
+    };
+
+    return (
+        <div className={styles.settingsWrapper}>
+            <div className={styles.settingsCard}>
+                <Animation type="normal">
+                    <FormBS initialValues={initialValue} validationSchema={userSettingsValidation}
+                    handlerFunction={async (user, action) => {
+                        console.log(user);
+                        action.resetForm();
+                    }}
+                    >
+                        <Form>
+                            {/* Name */}
+                            <div className="form-group mb-3">
+                                <label htmlFor="name" className={styles.label}> Name </label>
+                                <Field type="text" name="name" className={`form-control ${styles.input}`} placeholder="Enter Name" />
+                                <span className={styles.errorMsg}> <ErrorMessage name="name" /> </span>
+                            </div>
+
+                            {/* Username */}
+                            <div className="form-group mb-3">
+                                <label htmlFor="username" className={styles.label}> Username </label>
+                                <Field type="text" name="username" className={`form-control ${styles.input}`} placeholder="Enter Username" />
+                                <span className={styles.errorMsg}> <ErrorMessage name="username" /> </span>
+                            </div>
+
+                            {/* Password */}
+                            <div className="form-group mb-3">
+                                <label htmlFor="password" className={styles.label}> Password </label>
+                                <Field type="password" name="password" className={`form-control ${styles.input}`} placeholder="Enter Password" />
+                                <span className={styles.errorMsg}> <ErrorMessage name="password" /> </span>
+                            </div>              
+
+                            {/* Confirm Password */}
+                            <div className="form-group mb-3">
+                                <label htmlFor="cpassword" className={styles.label}> Confirm Password </label>
+                                <Field type="password" name="cpassword" className={`form-control ${styles.input}`} placeholder="Re-Enter Password" />
+                                <span className={styles.errorMsg}> <ErrorMessage name="cpassword" /> </span>
+                            </div>
+
+                            <hr />
+                            <i className='text-secondary mb-5 ms-1'>  Note: Leave the password fields blank to keep your current password. </i>
+
+                            {/* Save Changes */}
+                            <div className='d-grid mt-2'>
+                                <Button type="submit"> Save Changes </Button>
+                            </div>
+                        </Form>
+                    </FormBS>
+                </Animation>
+            </div>
+        </div>
+    );
+}
+
+export default UserSettings;
