30318494c83c57a0e280af75d834476b1335bf11
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-13 17:15:10 +0500

    Added axios interceptors to inject token in headers

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -4,7 +4,7 @@ import { ApiError } from "./ApiError";
 import { ApiResponse } from "./ApiResponse";
 
 // Create instance
-const api = axios.create({
+const client = axios.create({
     baseURL: backendURL,
     withCredentials: true,
     withXSRFToken: true, 
@@ -13,7 +13,7 @@ const api = axios.create({
 });
 
 // Request interceptor
-api.interceptors.request.use((request) => {
+client.interceptors.request.use((request) => {
     const csrfToken = localStorage.getItem("csrfToken");     // Get CSRF-Token
     if(csrfToken) request.headers["CSRF-Token"] = csrfToken; // Inject token in header
     return request;
@@ -22,10 +22,10 @@ api.interceptors.request.use((request) => {
 });
 
 // Response interceptor
-api.interceptors.response.use((response) => {
+client.interceptors.response.use((response) => {
     return ApiResponse(response);
 }, (error) => {
     return Promise.reject(ApiError(error));
 });
 
-export default api;
+export default client;
