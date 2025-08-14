3609d1d8d94f30972474ce254dbf5e5e7c9558f5
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-14 21:25:01 +0500

    Enable reinitialization

--- client/src/components/Form/index.jsx
+++ client/src/components/Form/index.jsx
@@ -5,7 +5,7 @@ function FormBS({ children, initialValues, validationSchema, handlerFunction })
 {
     return (
         <div className={styles.formContainer}>
-            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handlerFunction} validateOnBlur={true} validateOnChange={true}>
+            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handlerFunction} enableReinitialize={true}>
                 { children }
             </Formik>
         </div>
