4faf14a2684eeb56c740836f094c2b40bb4cb602
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-15 19:05:03 +0500

    Optimized protected route code

--- client/src/pages/security/ProtectedRoutes/index.jsx
+++ client/src/pages/security/ProtectedRoutes/index.jsx
@@ -2,16 +2,30 @@ import { useAuth } from '../../../context/auth';
 import { Outlet } from 'react-router-dom';
 import styles from './style.module.css';
 
-function ProtectedRoute() 
+function ProtectedRoute({ roles = [] }) 
 {
-    const { isLoggedIn } = useAuth();
-    if(isLoggedIn === null) return "";
+    const { user, isLoggedIn } = useAuth();
 
-    if(isLoggedIn === false) return (
-        <div className={styles.wrapper}>
-            <h2> 401 - UNAUTHORIZED </h2>
-        </div>
-    );
+    // Check authentication
+    if(isLoggedIn === null) return "";
+    if(isLoggedIn === false) 
+    {
+        return (
+            <div className={styles.wrapper}>
+                <h2> 401 - UNAUTHORIZED </h2>
+            </div>
+        );
+    }
+
+    // Check role based permissions
+    if(!roles.includes(user?.role))
+    {
+        return (
+            <div className={styles.wrapper}>
+                <h2> 403 - FORBIDDEN </h2>
+            </div>            
+        );
+    }
     
     return <Outlet />;
 }
