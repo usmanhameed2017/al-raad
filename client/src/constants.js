20b261248fdef2e35d36a442d32277bfb3ef4cb3
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-20 17:08:38 +0500

    Removed datatable theme

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,3 @@
-import { createTheme } from "react-data-table-component";
-
 // Backend URL
 export const backendURL = import.meta.env.VITE_BACKEND_URL;
 
@@ -30,12 +28,3 @@ export const isArrayHaveData = (arrayData) => {
     if(arrayData && Array.isArray(arrayData) && arrayData?.length > 0) return true;
     return false
 };
-
-// Data table theme
-export const datatableTheme = createTheme("alRaad",{
-    text: { primary: "#e5e7eb", secondary: "#cbd5e1" },
-    background: { default: "#0e0f16" },
-    context: { background: "#0dcdbc", text: "#0e0f16" },
-    divider: { default: "rgba(13,205,188,0.20)" },
-    action: { button: "rgba(13,205,188,0.9)", hover: "rgba(13,205,188,0.08)", disabled: "rgba(255,255,255,0.3)" },
-}, "dark");
