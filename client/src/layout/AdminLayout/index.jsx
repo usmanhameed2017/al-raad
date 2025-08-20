fee645c6df64b4669371f545eaa6ebdd7f045f4e
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-21 02:24:14 +0500

    Refactor admin layout

--- client/src/layout/AdminLayout/index.jsx
+++ client/src/layout/AdminLayout/index.jsx
@@ -1,55 +1,13 @@
-import { useState } from "react";
-import { Outlet, Link } from "react-router-dom";
-import { FaTachometerAlt, FaUsers, FaCog, FaSignOutAlt, FaUserAlt, FaBook } from "react-icons/fa";
+import { Outlet } from "react-router-dom";
 import styles from "./style.module.css";
-import { useAuth } from "../../context/auth";
+import SideBar from "../../components/SideBar";
 
-function AdminLayout() {
-    const [isOpen, setIsOpen] = useState(true);
-    const { user, adminLogout } = useAuth();
-
+function AdminLayout() 
+{
     return (
         <div className={styles.container}>
             {/* Sidebar */}
-            <aside className={`${styles.sidebar} ${isOpen ? styles.open : styles.closed}`}>
-                {/* Admin name */}
-                <div className={styles.logo}>
-                    <FaUserAlt size={25} />
-                    {isOpen && <span> { user?.name } </span>}
-                </div>
-
-                <nav className={styles.nav}>
-                    {/* Dashboard */}
-                    <Link to="/admin/dashboard" className={styles.navItem}>
-                        <FaTachometerAlt size={25} /> {isOpen && <span> Dashboard </span>}
-                    </Link>
-
-                    {/* Books */}
-                    <Link to="/admin/books" className={styles.navItem}>
-                        <FaBook size={25} /> {isOpen && <span> Books </span>}
-                    </Link>                    
-
-                    {/* Users */}
-                    <Link to="/admin/users" className={styles.navItem}>
-                        <FaUsers size={25} /> {isOpen && <span> Users </span>}
-                    </Link>
-
-                    {/* Settings */}
-                    <Link to="/admin/settings" className={styles.navItem}>
-                        <FaCog size={25} /> {isOpen && <span> Settings </span>}
-                    </Link>
-
-                    {/* Logout */}
-                    <Link onClick={adminLogout} className={styles.navItem}>
-                        <FaSignOutAlt size={25} /> {isOpen && <span> Logout </span>}
-                    </Link>
-                </nav>
-
-                {/* Toggle Button */}
-                <button className={styles.toggleBtn} onClick={() => setIsOpen(!isOpen)}>
-                    {isOpen ? "«" : "»"}
-                </button>
-            </aside>
+            <SideBar />
 
             {/* Main Content */}
             <main className={styles.main}>
