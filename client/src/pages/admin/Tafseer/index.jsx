d0c8134ac9fb062649faf5e0ba03274db8b28b25
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-21 19:22:57 +0500

    Created tafseer section on admin panel side

---
+++ client/src/pages/admin/Tafseer/index.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import { getRequest } from '../../../api/request';
+import ReactDataTable from '../../../components/DataTable';
+import Button from '../../../components/Button';
+import { useAuth } from '../../../context/auth';
+import Animation from '../../../components/Animation';
+
+function Tafseer() 
+{
+    // States
+    const [currentPage, setCurrentPage] = useState(1);
+    const [limit, setLimit] = useState(10); // Records per page
+    const [data, setData] = useState({ docs: [], totalDocs: 0, pagingCounter: 1 });
+    const [search, setSearch] = useState("");
+    const [debouncedSearch, setDebouncedSearch] = useState("");
+
+    // Global state loader
+    const { setLoading } = useAuth();
+    
+    // Debounce technique
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedSearch(search);
+            setCurrentPage(1);
+        }, 500);
+        return () => clearTimeout(timer);
+    }, [search]); 
+    
+    // Fetch data on page load and on search
+    useEffect(() => {
+        setLoading(true);
+        getRequest(`/tafseer?page=${currentPage}&limit=${limit}&search=${debouncedSearch}`)
+        .then((response) => setData(response.data))
+        .catch(error => console.log("Error:", error.message))
+        .finally(() => setLoading(false));
+    }, [currentPage, limit, debouncedSearch]);
+
+    // Columns
+    const columns = [
+        { name: "SR.NO", cell: (row, index) => (data?.pagingCounter || 0) + index, sortable: true, width: "120px" },
+        { name: "Surah Name", selector: row => row.surahName, sortable: true },
+        { name: "Ayah", selector: row => row.ayah, sortable: true },
+        { name: "Tafseer", selector: row => row.tafseer?.substring(0,50) || "", wrap:true },
+        { name: "Language", selector: row => row.language, wrap:true },
+        { name: "Uploaded By", selector: row => row.uploadedBy.name, wrap:true }
+    ];  
+
+    return (
+        <>
+            <Animation type="table">
+                <ReactDataTable 
+                title={`Tafseer`} 
+                columns={columns} 
+                data={data} 
+                setCurrentPage={setCurrentPage}
+                search={search}
+                setSearch={setSearch}
+                limit={limit}
+                setLimit={setLimit}
+                />
+            </Animation>
+        </>
+    );
+}
+
+export default Tafseer;
