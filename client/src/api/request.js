e24f3152f0a51b5e4e4ec1afcef15390d752aa4c
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-24 00:49:40 +0500

    Created a wrapper for post request

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -11,4 +11,19 @@ export const getRequest = async (url) => {
     {
         return error;
     }
-}; 
+};
+
+// Post request
+export const postRequest = async (url, payload, fileAttachment = false) => {
+    try 
+    {
+        let options = {};
+        if(fileAttachment) options = { headers: { "Content-Type": "multipart/form-data" } };
+        const response = await client.post(url, payload, options);
+        return response;
+    } 
+    catch(error) 
+    {
+        return error;
+    }
+};
