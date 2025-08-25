7230e29eafeb13f3ea1def13d71c5bfead6c4b91
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-25 14:22:58 +0500

    Included custom sized

--- client/src/components/Loader/index.jsx
+++ client/src/components/Loader/index.jsx
@@ -1,13 +1,37 @@
 import { FaSync } from 'react-icons/fa';
 import styles from './style.module.css';
 
-function Loader({ text }) 
+function Loader({ size = "small", text }) 
 {
-    return (
-        <h3 className='text-center fw-bold textTheme'> 
-            <FaSync className={styles.spin} size={30} /> { text } 
-        </h3>
-    );
+    // Big
+    if(size.trim().toLowerCase() === "big")
+    {
+        return (
+            <h2 className='text-center fw-bold textTheme'> 
+                <FaSync className={styles.spin} size={30} /> { text } 
+            </h2>
+        );
+    }
+
+    // Medium
+    if(size.trim().toLowerCase() === "medium")
+    {
+        return (
+            <h4 className='text-center fw-bold textTheme'> 
+                <FaSync className={styles.spin} size={30} /> { text } 
+            </h4>
+        );
+    }
+
+    // Small
+    if(size.trim().toLowerCase() === "small")
+    {
+        return (
+            <h6 className='text-center fw-bold textTheme'> 
+                <FaSync className={styles.spin} size={30} /> { text } 
+            </h6>
+        );
+    }    
 }
 
 export default Loader;
