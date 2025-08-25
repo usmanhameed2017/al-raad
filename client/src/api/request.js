14631a22d0c0ba6b393652bca6d7da5ef15f3be0
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-25 15:38:20 +0500

    Added message options

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -1,4 +1,5 @@
 import client from '../utils/axios';
+import { showSuccess, showError } from '../utils/toasterMessage';
 
 // Get request
 export const getRequest = async (url) => {
@@ -9,21 +10,23 @@ export const getRequest = async (url) => {
     } 
     catch(error) 
     {
-        return error;
+        throw error;
     }
 };
 
 // Post request
-export const postRequest = async (url, payload, fileAttachment = false) => {
+export const postRequest = async (url, payload, fileAttachment = false, enableSuccessMessage = true, enableErrorMessage = true) => {
     try 
     {
         let options = {};
         if(fileAttachment) options = { headers: { "Content-Type": "multipart/form-data" } };
         const response = await client.post(url, payload, options);
+        if(enableSuccessMessage) showSuccess(response.message);
         return response;
     } 
     catch(error) 
     {
-        return error;
+        if(enableErrorMessage) showError(error.message);
+        throw error;
     }
 };
