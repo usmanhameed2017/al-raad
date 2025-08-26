43b7f9690d80d2283f7938194edbd72e391b271a
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-26 17:20:58 +0500

    Removed unnecessary transitions

--- client/src/pages/marketing/Login/AccountActivation.jsx
+++ client/src/pages/marketing/Login/AccountActivation.jsx
@@ -1,67 +1,50 @@
 import { Form, Field, ErrorMessage } from 'formik';
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
 import FormBS from "../../../components/Form";
 import Button from "../../../components/Button";
 import styles from './style.module.css';
 import { FaLock } from 'react-icons/fa';
 import * as Yup from 'yup';
-import { showError, showSuccess } from '../../../utils/toasterMessage';
-import client from '../../../utils/axios';
+import { patchRequest } from '../../../api/request';
+import Animation from '../../../components/Animation';
 
 function AccountActivation() 
 {
+    // Navigator
     const navigate = useNavigate();
-    const formVariants = {
-        hidden: { opacity: 0, y: 50 },
-        visible: {
-            opacity: 1,
-            y: 0,
-            transition: {
-                duration: 0.6,
-                ease: "easeOut",
-            },
-        }
+
+    // Initial values
+    const initialValues = {
+        activationCode: ""
     };
+
+    // Validation schema
+    const validationSchema = Yup.object({
+        activationCode:Yup.string()
+        .min(6, "Activation code must be at least 6 characters long")
+        .max(12, "Activation code must not be longer than 12 characters")
+        .required("Activation code is required")
+    });
 
     return (
         <div className={styles.loginPage}>
-            <motion.div
-                className={styles.glassCard}
-                initial={{ opacity: 0, scale: 0.95 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.4, ease: "easeOut" }}
-                style={{
-                    maxWidth: "500px",
-                    margin: "3rem auto",
-                    marginTop: '200px',
-                    padding: "2rem",
-                    borderRadius: "15px",
-                }}>
+            <div className={styles.glassCard} 
+            style={{ maxWidth: "500px", margin: "3rem auto", marginTop: '200px', padding: "2rem", borderRadius: "15px"}}>
 
-                <FormBS initialValues={{ activationCode:"" }} validationSchema={Yup.object({
-                    activationCode:Yup.string()
-                    .min(6, "Activation code must be at least 6 characters long")
-                    .max(12, "Activation code must not be longer than 12 characters")
-                    .required("Activation code is required")
-                })}
-
-                // Handler function
+                <FormBS initialValues={initialValues} validationSchema={validationSchema}
                 handlerFunction={async (values, action) => {
                     try
                     {
-                        const response = await client.patch(`/user/accountActivation`, values);
-                        showSuccess(response.message);
+                        patchRequest(`/user/accountActivation`, values);
                         action.resetForm();
                         navigate("/login");
                     }
                     catch(error)
                     {
-                        showError(error.message);
+                        return error;
                     }
-                }}
-                >
-                    <motion.div variants={formVariants} initial="hidden" animate="visible">
+                }}>
+                    <Animation type="normal">
                         <Form>
                             <div>
                                 <h2> <FaLock size={25} /> Account Activation </h2> 
@@ -76,12 +59,12 @@ function AccountActivation()
                             <hr />
                             {/* Confirm */}
                             <div className="form-group d-grid mt-3">
-                                <Button type="submit" className="custom-btn w-100 mt-3">Confirm</Button>
+                                <Button type="submit" className="w-100 mt-3">Confirm</Button>
                             </div>
-                        </Form>
-                    </motion.div>
+                        </Form>                        
+                    </Animation>
                 </FormBS>
-            </motion.div>            
+            </div>                      
         </div>
     );
 }
