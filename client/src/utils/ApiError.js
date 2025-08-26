b2d426fb8537b94faa7a6d1b44abcd4548d83ca2
Usman Hameed <usmanhameed1790@gmail.com>
2025-08-26 15:00:55 +0500

    Included status code and stack trace properties in Api Error response

--- client/src/utils/ApiError.js
+++ client/src/utils/ApiError.js
@@ -1,6 +1,8 @@
 export const ApiError = (error) => {
     const data = error?.response?.data || null;
-    const success = error?.response?.data?.success || false;
-    const message = error?.response?.data?.message || null;
-    return { data, message, success };
+    const success = data?.success || false;
+    const message = data?.message || "An unknown error occur";
+    const statusCode = error?.response?.status || null;
+    const stack = error?.stack || null;
+    return { data, message, success, statusCode, stack };
 };
