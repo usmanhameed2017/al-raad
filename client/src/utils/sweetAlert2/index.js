200b0101d827721c8319ebd414cf2e66cee9afc9
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-08-27 00:28:43 +0500

    Created a reuseable sweet alert utility function

--- client/src/utils/sweetAlert2/index.js
+++ client/src/utils/sweetAlert2/index.js
@@ -1,18 +1,42 @@
 import swal from "sweetalert2";
 import "./style.css";
 
-export const confirmAlert = (title, text, fn) => {
-    swal.fire({
-        title: title,
-        text: text,
-        icon: "question",
-        showCloseButton:true,
-        showCancelButton: true,
-        confirmButtonText: "Yes, delete it!",
-        cancelButtonText: "Cancel",
-        allowOutsideClick:false
-    })
-    .then((result) => {
-        if(result.isConfirmed) fn();
-    });
+export const sweetAlert = (title, text, mode = "success", confirmButtonText = "OK", cancelButtonText = "Cancel", fn) => {
+    // Success
+    if(mode === "success")
+    {
+        return (
+            swal.fire({
+                title: title,
+                text: text,
+                icon: "success",
+                showCloseButton:true,
+                showCancelButton: true,
+                confirmButtonText: confirmButtonText || "OK",
+                cancelButtonText: cancelButtonText || "Cancel",
+                allowOutsideClick:false
+            })            
+        );
+    }
+
+    // Confirm
+    if(mode === "confirm")
+    {
+        return(
+            swal.fire({
+                title: title,
+                text: text,
+                icon: "question",
+                showCloseButton:true,
+                showCancelButton: true,
+                confirmButtonText: confirmButtonText || "Yes",
+                cancelButtonText: cancelButtonText || "Cancel",
+                allowOutsideClick:false
+            })
+            .then(async (result) => {
+                if(result.isConfirmed) await fn();
+            })
+        );
+    }
+
 };
