414a87c2db7dcabc6c1697444d1e8b5b0b71d58b
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-08-27 21:35:21 +0500

    Removed container for position control

--- client/src/components/Loader/index.jsx
+++ client/src/components/Loader/index.jsx
@@ -1,50 +1,37 @@
 import { FaSync } from 'react-icons/fa';
 import styles from './style.module.css';
-import { useAuth } from '../../context/auth';
 
 function Loader({ size = "small", text }) 
 {
-    // Extract global state loader
-    const { loading } = useAuth();
-    
-    if(loading)
+    // Big
+    if(size.trim().toLowerCase() === "big")
     {
-        // Big
-        if(size.trim().toLowerCase() === "big")
-        {
-            return (
-                <div className={styles.container}>
-                    <h3 className='text-center fw-bold textTheme'> 
-                        <FaSync className={styles.spin} size={30} /> { text } 
-                    </h3>
-                </div>
-            );
-        }
-
-        // Medium
-        if(size.trim().toLowerCase() === "medium")
-        {
-            return (
-                <div className={styles.container}>
-                    <h4 className='text-center fw-bold textTheme'> 
-                        <FaSync className={styles.spin} size={30} /> { text } 
-                    </h4>
-                </div>
-            );
-        }
-
-        // Small
-        if(size.trim().toLowerCase() === "small")
-        {
-            return (
-                <div className={styles.container}>
-                    <h6 className='text-center fw-bold textTheme'> 
-                        <FaSync className={styles.spin} size={30} /> { text } 
-                    </h6>
-                </div>
-            );
-        } 
+        return (
+            <h3 className='text-center fw-bold textTheme'> 
+                <FaSync className={styles.spin} size={30} /> { text } 
+            </h3>
+        );
     }
+
+    // Medium
+    if(size.trim().toLowerCase() === "medium")
+    {
+        return (
+            <h4 className='text-center fw-bold textTheme'> 
+                <FaSync className={styles.spin} size={30} /> { text } 
+            </h4>
+        );
+    }
+
+    // Small
+    if(size.trim().toLowerCase() === "small")
+    {
+        return (
+            <h6 className='text-center fw-bold textTheme'> 
+                <FaSync className={styles.spin} size={30} /> &nbsp; { text } 
+            </h6>
+        );
+    } 
 }
 
 export default Loader;
