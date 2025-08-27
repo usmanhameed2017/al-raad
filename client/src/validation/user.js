25414d9ce78204a0a7369a0336e8755ec2f3a5a4
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-08-27 22:49:38 +0500

    Removed signin resources

--- client/src/validation/user.js
+++ client/src/validation/user.js
@@ -7,38 +7,6 @@ const imageTypeChecker = (value) => !value || allowedImageTypes.includes(value?.
 // Image size checker
 const imageSizeChecker = (value) => !value || value.size && value.size <= 5000000
 
-export const loginValidation = Yup.object({
-    username:Yup.string()
-    .required('Username is required'),
-
-    password:Yup.string()
-    .required('Password is required')
-});
-
-export const signupValidation = Yup.object({
-    name:Yup.string()
-    .min(3, "Name must be at least 3 characters long")
-    .max(20, "Name must not be longer than 20 characters")
-    .required("Name is required"),
-
-    email:Yup.string()
-    .lowercase()
-    .email('Invalid email')
-    .required("Email is required"),
-
-    username:Yup.string()
-    .lowercase()
-    .required('Username is required'),
-
-    password:Yup.string()
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/, "Enter strong password")
-    .required('Password is required'),
-
-    cpassword:Yup.string()
-    .oneOf([Yup.ref('password'), null], "Password & confirm password must be identical")
-    .required('Confirm password is required'),
-});
-
 // Update password validation
 export const updatePasswordValidation = Yup.object({
     oldPassword:Yup.string()
