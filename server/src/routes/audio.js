4a181f5dde934704996dc45c0d49096ea3e2aa16
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-08-28 15:22:55 +0500

    Included CSRF token middleware to prevent unintended form submission

--- server/src/routes/audio.js
+++ server/src/routes/audio.js
@@ -1,17 +1,18 @@
 const { createAudio, fetchAudios, fetchSingleAudio, updateAudio, deleteAudio } = require("../controllers/audio");
 const { authentication, authorization } = require("../middlewares/auth");
+const csrfProtection = require("../middlewares/csrfToken");
 const upload = require("../middlewares/multer");
 
 // Audio router instance
 const audioRouter = require("express").Router();
 
 audioRouter.route("/")
-.post(authentication, authorization(["Admin"]), upload.single("url"), createAudio) // Create audio
+.post(csrfProtection, authentication, authorization(["Admin"]), upload.single("url"), createAudio) // Create audio
 .get(fetchAudios) // Fetch all audios
 
 audioRouter.route("/:id")
 .get(fetchSingleAudio) // Fetch single audio
-.put(authentication, authorization(["Admin"]), upload.single("url"), updateAudio) // Update audio
+.put(csrfProtection, authentication, authorization(["Admin"]), upload.single("url"), updateAudio) // Update audio
 .delete(authentication, authorization(["Admin"]), deleteAudio); // Delete audio
 
 module.exports = audioRouter;
