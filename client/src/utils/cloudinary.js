e6eebba53f8930528c80f3187001dc5bbe5fdfba
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-08-29 19:29:36 +0500

    Created a helper function to make file optimized via url

--- client/src/utils/cloudinary.js
+++ client/src/utils/cloudinary.js
@@ -1,8 +1,22 @@
 import { Cloudinary } from '@cloudinary/url-gen';
 import { cloudinaryCloudName } from '../constants';
+import { bitRate } from '@cloudinary/url-gen/actions/transcode';
+import { format, quality } from '@cloudinary/url-gen/actions/delivery';
 
 const cld = new Cloudinary({
     cloud: { cloudName: cloudinaryCloudName }
 });
+
+export const getOptimized = (url) => {
+    // Extract public ID
+    const publicId = `al-raad/audios/${url.split("/al-raad/audios/").pop().split(".")[0]}`;
+
+    // Cloudinary treats audio as video resource
+    const audio = cld.video(publicId)
+    .transcode(bitRate('128k'))
+    .delivery(format('mp3'), quality('auto'));
+
+    return audio.toURL();
+}
 
 export default cld;
