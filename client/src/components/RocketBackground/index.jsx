76416f7d6cbf8cdeb122eb3d1fb33661ede5116c
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-08-29 23:41:22 +0500

    Created a rocket background component

---
+++ client/src/components/RocketBackground/index.jsx
@@ -0,0 +1,26 @@
+import styles from "./style.module.css";
+
+function RocketBackground() 
+{
+    return (
+        <>
+            <span className={styles.rocket}></span>
+            <span className={styles.rocket}></span>
+            <span className={styles.rocket}></span>
+            <span className={styles.rocket}></span>
+            <span className={styles.rocket}></span> 
+            <span className={styles.rocket}></span>  
+            <span className={styles.rocket}></span>
+            <span className={styles.rocket}></span> 
+            <span className={styles.rocket}></span>   
+            <span className={styles.rocket}></span>
+            <span className={styles.rocket}></span> 
+            <span className={styles.rocket}></span>  
+            <span className={styles.rocket}></span>
+            <span className={styles.rocket}></span> 
+            <span className={styles.rocket}></span>                                                                    
+        </>
+    );
+}
+
+export default RocketBackground;
