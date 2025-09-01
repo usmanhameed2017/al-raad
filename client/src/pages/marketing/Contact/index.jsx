3c8123d6b401da3a2163efa5362ed2ff590cd669
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-01 19:28:48 +0500

    Exclude extra fields

--- client/src/pages/marketing/Contact/index.jsx
+++ client/src/pages/marketing/Contact/index.jsx
@@ -3,9 +3,29 @@ import styles from './style.module.css';
 import Button from '../../../components/Button';
 import FormBS from '../../../components/Form';
 import { ErrorMessage, Field, Form } from 'formik';
+import * as Yup from "yup";
 
 function Contact() 
 {
+    // Initial values
+    const initialValues = {
+        subject:"",
+        message:""
+    };
+
+    // Validation schema
+    const validationSchema = Yup.object({
+        // Subject
+        subject: Yup.string()
+        .required("Please select subject"),
+        
+        // Message
+        message: Yup.string()
+        .min(10, "Message must be at least 10 characters long")
+        .max(9999, "Message must not be longer than 9999 characters")
+        .required("Message is required"),         
+    });
+
     return (
         <div className={styles.contactWrapper}>
             <img src="/public/contactus.jpg" alt="background" className={styles.bgImage} />
@@ -22,26 +42,29 @@ function Contact()
                         Have a question, suggestion, or just want to say hello? Fill out the form below.
                     </p>
 
-                    <FormBS>
+                    <FormBS initialValues={initialValues} validationSchema={validationSchema}
+                    handlerFunction={(values, action) => {
+                        console.log(values);
+                        action.resetForm();
+                    }}>
                         <Form className={styles.contactForm}>
-                            {/* Name */}
+                            {/* Subject Options */}
                             <div className={styles.formGroup}>
-                                <label htmlFor="name" className={styles.formLabel}>Name</label>
-                                <Field type='text' name='name' className={`${styles.inputField}`} placeholder="Your Name" />
-                                <span className={styles.textDanger}><ErrorMessage name="name" /></span>
-                            </div>
-
-                            {/* Email */}
-                            <div className={styles.formGroup}>
-                                <label htmlFor="email" className={styles.formLabel}>Email</label>
-                                <Field type='email' name='email' className={styles.inputField} placeholder="Your Email" />
-                                <span className={styles.textDanger}><ErrorMessage name="email" /></span>
-                            </div>
+                                <label htmlFor="subject" className={styles.formLabel}>Subject</label>
+                                <Field as='select' name='subject' className={`${styles.inputField} mb-1`}> 
+                                    <option value="" className='text-black'> Select </option>
+                                    <option value="Feedback" className='text-black'> Feedback </option>
+                                    <option value="Suggestion" className='text-black'> Suggestion </option>
+                                    <option value="Question" className='text-black'> Question </option>
+                                    <option value="Business Query" className='text-black'> Business Query </option>
+                                </Field>
+                                <span className={styles.textDanger}><ErrorMessage name="subject" /></span>
+                            </div>                            
 
                             {/* Message */}
                             <div className={styles.formGroup}>
                                 <label htmlFor="message" className={styles.formLabel}>Message</label>
-                                <Field as="textarea" name="message" className={styles.textarea} placeholder="Your Message" />
+                                <Field as="textarea" name="message" className={`${styles.textarea}`} placeholder="Your Message" />
                                 <span className={styles.textDanger}><ErrorMessage name="message" /></span>
                             </div>
 
