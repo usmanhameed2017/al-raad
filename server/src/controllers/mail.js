da0b3690d0b2585571dea8b5e53db457d4e92d19
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-01 19:43:31 +0500

    Created a controller for mails

---
+++ server/src/controllers/mail.js
@@ -0,0 +1,99 @@
+const Mail = require("../models/mail");
+const ApiError = require("../utils/ApiError");
+const ApiResponse = require("../utils/ApiResponse");
+const { isValidObjectId } = require("mongoose");
+
+// Send mail
+const sendMail = async (request, response) => {
+    try 
+    {
+        request.body.name = request.user?.name;
+        request.body.email = request.user?.email;
+        request.body.mailedBy = request.user?._id;
+        // const mail = await Mail.create(request.body);
+        // return response.status(201).json(new ApiResponse(201, mail, "Email has been sent successfully"));
+    } 
+    catch (error) 
+    {
+        throw new ApiError(500, error.message);
+    }
+};
+
+// Fetch all mails
+const fetchMails = async (request, response) => {
+    const { page = 1, limit = 10, search = "" } = request.query;
+
+    // Paging options
+    const options = {
+        page:parseInt(page),
+        limit:parseInt(limit),
+        sort: { createdAt: -1 },
+        populate: { path:"mailedBy", select:"name" }
+    };    
+
+    try 
+    {
+        let query = {};
+
+        // If search keyword provided
+        if (search && search.trim() !== "") 
+        {
+            query = {
+                $or: [
+                    { name: { $regex: search.trim(), $options: "i" } },
+                    { subject: { $regex: search.trim(), $options: "i" } }
+                ]
+            };
+        }
+
+        // Execute query
+        const result = await Mail.paginate(query, options);
+
+        // If page size is greater than total pages
+        if(page > result.totalPages) throw new ApiError(404, "Mail not found");
+
+        return response.status(200).json(new ApiResponse(200, result, "All mails has been fetched successfully"));        
+    } 
+    catch (error) 
+    {
+        throw new ApiError(404, error.message);
+    }
+};
+
+// Fetch single mail
+const fetchSingleMail = async (request, response) => {
+    const id = request.params?.id || null;
+    if(!id) throw new ApiError(404, "Mail ID is missing");
+    if(!isValidObjectId(id)) throw new ApiError(400, "Invalid mongodb ID");
+
+    try 
+    {
+        const mail = await Mail.findById(id);
+        if(!mail) throw new ApiError(404, "Mail not found");
+        return response.status(200).json(new ApiResponse(200, mail, "Mail has been fetched successfully"));
+    }
+    catch(error) 
+    {
+        throw new ApiError(500, error.message);
+    }
+};
+
+// Delete mail
+const deleteMail = async (request, response) => {
+    const id = request.params?.id || null;
+    if(!id) throw new ApiError(404, "Mail ID is missing");
+    if(!isValidObjectId(id)) throw new ApiError(400, "Invalid mongodb ID");
+
+    try 
+    {
+        const mail = await Mail.findByIdAndDelete(id);
+        if(!mail) throw new ApiError(404, "Mail not found");
+        return response.status(200).json(new ApiResponse(200, mail, "Mail has been deleted successfully"));
+    }
+    catch(error) 
+    {
+        throw new ApiError(500, error.message);
+    }
+};
+
+module.exports = { sendMail, fetchMails, fetchSingleMail, deleteMail };
