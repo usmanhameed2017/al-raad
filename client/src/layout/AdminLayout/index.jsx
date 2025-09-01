ec647622c2b7cbaee2cd4cd18e70cde313f9f3f0
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-01 14:03:28 +0500

    Removed background animation

--- client/src/layout/AdminLayout/index.jsx
+++ client/src/layout/AdminLayout/index.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import styles from "./style.module.css";
 import SideBar from "../../components/SideBar";
 import Animation from "../../components/Animation";
-import RocketBackground from "../../components/RocketBackground";
 
 function AdminLayout() 
 {
@@ -13,7 +12,6 @@ function AdminLayout()
 
             {/* Main Content */}
             <main className={styles.main}>
-                <RocketBackground />
                 <Animation type="page">
                     <Outlet />
                 </Animation>
