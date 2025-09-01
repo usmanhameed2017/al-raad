aa88db0ee63c310c5cebc0f89a62fb67748a598c
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-01 19:51:33 +0500

    Refactor error messages

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -4,10 +4,10 @@ const { verifyAccessToken } = require("../utils/auth");
 // Verify authentication
 const authentication = (request, response, next) => {
     const token = request.signedCookies?.accessToken || request.headers?.["authorization"]?.split(" ")?.[1] || null;
-    if(!token) throw new ApiError(401, "Access token is missing");
+    if(!token) throw new ApiError(401, "Login required!");
 
     const user = verifyAccessToken(token);
-    if(!user) throw new ApiError(401, "Unauthenticated");
+    if(!user) throw new ApiError(401, "Unauthenticated! Invalid access token");
 
     request.user = user || null;
     return next();
