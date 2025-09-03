35a0778c4184e890b72222b63fb7832add174b71
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-03 19:47:55 +0500

    Implemented active link functionality

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,5 +1,5 @@
 import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./style.module.css";
 import { getUser } from "../../constants";
 import { useAuth } from "../../context/auth";
@@ -13,30 +13,30 @@ function NavbarBS()
     return (
         <Navbar variant="dark" expand="lg" fixed="top" className={styles.navbar}>
             <Container fluid>
-                <Navbar.Brand as={Link} to="/" className={styles.logo}> Al-Ra’ad </Navbar.Brand>
+                <Navbar.Brand as={NavLink} to="/" className={styles.logo}> Al-Ra’ad </Navbar.Brand>
                 <Navbar.Toggle aria-controls="main-navbar" className={styles.hamburger} />
                 <Navbar.Collapse id="main-navbar">
                     <Nav className={`ms-auto ${styles.NavLinks}`}>
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+                        <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
                         {
-                            userData && ( <Nav.Link as={Link} to="/tafseer">Tafseer</Nav.Link> )
+                            userData && ( <Nav.Link as={NavLink} to="/tafseer">Tafseer</Nav.Link> )
                         }
-                        <Nav.Link as={Link} to="/books">Books</Nav.Link>
-                        <Nav.Link as={Link} to="/audios">Audios</Nav.Link>
-                        {/* <Nav.Link as={Link} to="/videos">Videos</Nav.Link> */}
+                        <Nav.Link as={NavLink} to="/books">Books</Nav.Link>
+                        <Nav.Link as={NavLink} to="/audios">Audios</Nav.Link>
+                        {/* <Nav.Link as={NavLink} to="/videos">Videos</Nav.Link> */}
                         {
                             userData ? 
                             (
                                 <NavDropdown title={ userData?.name || "Account" } className={styles.userDropdown}>
-                                    <NavDropdown.Item as={Link} to="/settings">Settings</NavDropdown.Item>
+                                    <NavDropdown.Item as={NavLink} to="/settings">Settings</NavDropdown.Item>
                                     <NavDropdown.Item onClick={userLogout}>Logout</NavDropdown.Item>
                                 </NavDropdown>
                             )
                             :
                             (
-                                <Nav.Link as={Link} to="/login">Signin</Nav.Link>
+                                <Nav.Link as={NavLink} to="/login">Signin</Nav.Link>
                             )
                         }
                     </Nav>
