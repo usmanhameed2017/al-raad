f80994f34520d85e93cbe87f61df3f640b41ef9c
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-03 13:25:43 +0500

    Fixed bug of invalid mongodb id

--- client/src/pages/admin/ViewMail/index.jsx
+++ client/src/pages/admin/ViewMail/index.jsx
@@ -1,13 +1,63 @@
-import { useLocation } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import EmailTemplate from '../../../components/EmailTemplate';
+import { useState, useEffect } from 'react';
+import { useAuth } from '../../../context/auth';
+import { getRequest } from '../../../api/request';
+import Loader from '../../../components/Loader';
+import { Row, Col } from 'react-bootstrap';
+import Animation from '../../../components/Animation';
 
 function ViewMail() 
 {
-    const { data } = useLocation().state;
-    const { name = "", email = "", subject = "", message = "" } = data;
+    const { id } = useParams();
+    const [mail, setMail] = useState({});
+    const [message, setMessage] = useState("");
+    const { loading } = useAuth();
+    const navigate = useNavigate();
+
+    // For better UX
+    useEffect(() => {
+        let timer;
+        if(!id) timer = setTimeout(() => setMessage("404 - Mail Not Found"), 700);
+        return () => clearTimeout(timer);
+    },[]);
+
+    // Fetch book on page load
+    useEffect(() => {
+        getRequest(`/mail/${id}`)
+        .then(response => setMail(response.data))
+        .catch(() => {
+            setMail({});
+            navigate("/admin/mails");
+        });
+    }, [id]);
+
     return (
         <>
-            <EmailTemplate name={name} email={email} subject={subject} body={message} />
+        {/* Loader */}
+        { loading && ( <div style={{ marginTop:"200px" }}> <Loader size='big' text="Loading" /> </div> ) } 
+        {
+            mail?._id ? 
+            (
+                // Mail
+                <Row>
+                    <Animation type={`3d`}>
+                        <Col>
+                            <EmailTemplate name={mail?.name} email={mail?.email} subject={mail?.subject} body={mail?.message} />
+                        </Col>                        
+                    </Animation>
+                </Row>
+            )
+            :
+            ( 
+                // No mail found
+                <Row>
+                    <Col>
+                        <h1 className='textTheme fw-bold text-center mt-5'> { message } </h1> 
+                    </Col>
+                </Row>
+            )
+        }
         </>
     );
 }
