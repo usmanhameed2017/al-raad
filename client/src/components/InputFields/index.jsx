e3371a5ad51ce5db50d43c7d0969eb7b5356354b
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-05 16:30:26 +0500

    Created an optimized and reuseable input component that works perfectly with formik and yup library

---
+++ client/src/components/InputFields/index.jsx
@@ -0,0 +1,76 @@
+import { Field, ErrorMessage, useFormikContext } from 'formik';
+import { useState } from 'react';
+import styles from "./style.module.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+function Input({ type, name, className, placeholder, required = false, accept, rows, list, children }) 
+{
+    // For handling files
+    const { setFieldValue } = useFormikContext();
+
+    // State to toggle show and hide password
+    const [showPassword, setShowPassword] = useState(false);
+
+    // Password
+    if(type === "password") return (
+        <>
+            {/* Wrapper */}
+            <div className={styles.passwordFieldWrapper}>
+                {/* Field */}
+                <Field type={showPassword ? "text" : "password"} name={name} className={`${className} pe-5`} placeholder={placeholder} required={required} />
+
+                {/* Icon */}
+                <span className={styles.icon} onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? <FaEyeSlash size={22} /> : <FaEye size={22} />}
+                </span>
+            </div> 
+            <span className={styles.errorMessage}> <ErrorMessage name={name} /> </span>          
+        </>
+    );
+
+    // File
+    if(type === "file") return (
+        <>
+            <input type={type} name={name} className={className} accept={accept} required={required}
+            onChange={ (e) => setFieldValue(name, e.target.multiple ? e.target.files : e.target.files[0]) } />
+            <span className={styles.errorMessage}> <ErrorMessage name={name} /> </span>
+        </>
+    );
+
+    // Text area
+    if(type === "textarea") return (
+        <>
+            <Field as={type} rows={rows} name={name} className={className} placeholder={placeholder} required={required} />
+            <span className={styles.errorMessage}> <ErrorMessage name={name} /> </span>
+        </>
+    );
+
+    // Select - (Dropdown list)
+    if(type === "select") return (
+        <>
+            <Field as={type} name={name} className={className} required={required}> 
+                { children }
+            </Field>
+            <span className={styles.errorMessage}> <ErrorMessage name={name} /> </span>
+        </>
+    );
+
+    // Datalist - (Dropdown list with manual typing)
+    if(type === "datalist") return (
+        <>
+            <Field type="text" name={name} list={list} className={className} placeholder={placeholder} required={required} />
+            <datalist id={list}> { children } </datalist>
+            <span className={styles.errorMessage}> <ErrorMessage name={name} /> </span>
+        </>
+    );
+
+    // Default input types: (Text, Number, Email, Search)
+    return (
+        <>
+            <Field type={type} name={name} className={className} placeholder={placeholder} required={required} />
+            <span className={styles.errorMessage}> <ErrorMessage name={name} /> </span>
+        </>
+    );
+}
+
+export default Input;
