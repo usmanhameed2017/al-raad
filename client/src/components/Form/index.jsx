2614a100b88b83b90ac202c34b723770223f5798
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-05 17:20:08 +0500

    Centralized Formik form

--- client/src/components/Form/index.jsx
+++ client/src/components/Form/index.jsx
@@ -1,13 +1,13 @@
 import { Formik, Form } from 'formik';
 import styles from "./style.module.css";
 
-function FormBS({ children, initialValues, validationSchema, handlerFunction })
+function FormBS({ children, initialValues, validationSchema, handlerFunction, className })
 {
     return (
         <div className={styles.formContainer}>
             <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handlerFunction} 
             enableReinitialize={true} validateOnBlur={false} validateOnChange={false}>
-                <Form> { children } </Form>
+                <Form className={className}> { children } </Form>
             </Formik>
         </div>
     );
