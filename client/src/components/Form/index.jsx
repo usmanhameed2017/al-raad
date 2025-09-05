f301bf02faa665f3333b56dd7919bd44cb1be989
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-05 16:29:30 +0500

    Wrapped a complete formik form into reuseable component

--- client/src/components/Form/index.jsx
+++ client/src/components/Form/index.jsx
@@ -1,18 +1,13 @@
-import { Formik } from 'formik';
+import { Formik, Form } from 'formik';
 import styles from "./style.module.css";
 
 function FormBS({ children, initialValues, validationSchema, handlerFunction })
 {
     return (
         <div className={styles.formContainer}>
-            <Formik 
-            initialValues={initialValues} 
-            validationSchema={validationSchema} 
-            onSubmit={handlerFunction} 
-            enableReinitialize={true}
-            validateOnBlur={false} 
-            validateOnChange={false}>
-                { children }
+            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handlerFunction} 
+            enableReinitialize={true} validateOnBlur={false} validateOnChange={false}>
+                <Form> { children } </Form>
             </Formik>
         </div>
     );
