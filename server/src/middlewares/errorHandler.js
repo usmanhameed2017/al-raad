2a3bfc50ff36f6b320fc3e7f198ea0a84eabf9f1
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-08 15:10:26 +0500

    Enhance error handling in middleware

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -6,10 +6,21 @@ const errorHandler = (error, request, response, next) => {
         const statusCode = error.statusCode || 500;
         const message = error.message || "Internal Server Error";
         const success = error.success || false;
-        const stack = error.stack || "";
+        const stack = process.env.NODE_ENV === "development" ? error.stack || "" : undefined
 
         return response.status(statusCode)
         .json({ statusCode, message, success, stack });
+    }
+    else
+    {
+        // Fallback for unexpected errors
+        return response.status(500)
+        .json({ 
+            statusCode: 500, 
+            message: error.message || "Something went wrong!", 
+            success: false,
+            stack: process.env.NODE_ENV === "development" ? error.stack || "" : undefined 
+        });        
     }
 };
 
