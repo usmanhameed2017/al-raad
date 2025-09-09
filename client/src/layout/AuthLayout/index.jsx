f74961c8f7de33d4c609866f8e090bbb076aff62
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-09 20:41:12 +0500

    Removed extra styling from auth layout

--- client/src/layout/AuthLayout/index.jsx
+++ client/src/layout/AuthLayout/index.jsx
@@ -1,17 +1,12 @@
 import { Outlet } from "react-router-dom";
-import styles from "./style.module.css";
 import Animation from "../../components/Animation";
 
 function AuthLayout() 
 {
     return (
-        <div className={styles.pageWrapper}>
-            <main className={styles.mainContent}>
-                <Animation type="page">
-                    <Outlet />
-                </Animation>
-            </main>
-        </div>
+        <Animation type="3d">
+            <Outlet />
+        </Animation>
     );
 }
 
