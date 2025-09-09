3021c6320c5fd986a89380ebeee860b064e2ec85
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-09 19:44:09 +0500

    Created a controller for security actions

---
+++ server/src/controllers/security.js
@@ -0,0 +1,106 @@
+const User = require("../models/user");
+const sendEmail = require("../service/mailer");
+const ApiError = require("../utils/ApiError");
+const ApiResponse = require("../utils/ApiResponse");
+const fs = require("fs");
+const path = require("path");
+const generateCode = require("../utils/generateCode");
+
+// Security Step-01 (Forgot Password)
+const forgotPassword = async (request, response) => {
+    try 
+    {
+        // Validate email
+        const email = request.params?.email || null;
+        if(!email) throw new ApiError(400, "Email is required");
+
+        // Get user
+        const user = await User.findOne({ email }).select("name email");
+        if(!user) throw new ApiError(404, "User not found associated with this email address");
+
+        // Generate verification code & expiry time
+        const { code:resetCode, expiresAt:resetCodeExpiresAt } = generateCode(15);
+
+        // Update reset code
+        const updateUser = await User.findByIdAndUpdate(user?._id, { resetCode, resetCodeExpiresAt }, { new:true }).select("-password");
+        if(!updateUser) throw new ApiError(404, "User not found");
+
+        // Get HTML template
+        const html = fs.readFileSync(path.resolve(__dirname, "../../public/forgotPassword.html"), "utf-8");
+
+        // Replace placeholders
+        const filledHtml = html
+        .replace('{{name}}', user?.name || "User")
+        .replace('{{resetCode}}', resetCode);
+
+        // Send email
+        const result = await sendEmail(email, "Reset Your Password", filledHtml);
+        if(!result) throw new ApiError(400, "Unable to send email"); 
+
+        return response.status(200)
+        .json(new ApiResponse(200, { _id:user?._id }, `We have sent you a reset code at your email ${email}`));
+    } 
+    catch(error) 
+    {
+        throw error;
+    }
+};
+
+// Security Step-02 (Verify Reset Code)
+const verifyResetCode = async (request, response) => {
+    try 
+    {
+        // Validate reset code
+        const { resetCode = null, _id = null } = request.params || {};
+        if(!resetCode) throw new ApiError(400, "Reset code is required");
+
+        // Get user
+        const user = await User.findOne({ _id, resetCode }, { new:true }).select("resetCode resetCodeExpiresAt");
+        if(!user) throw new ApiError(404, "Invalid reset code");
+
+        // Check reset code expiry
+        if(user?.resetCodeExpiresAt < Date.now()) throw new ApiError(400, "Reset code has expired");
+
+        // Update reset code and expiry time
+        const updateUser = await User.findByIdAndUpdate(user?._id, { resetCode:null, resetCodeExpiresAt:null }, { new:true });
+        if(!updateUser) throw new ApiError(404, "User not found");
+
+        return response.status(200).json(new ApiResponse(200, null, "Reset code has been verified! Please reset your password now"));
+    } 
+    catch(error) 
+    {
+        throw error;
+    }
+};
+
+// Security Step-03 (Reset Password)
+const resetPassword = async (request, response) => {
+    try 
+    {
+        const { _id, password, cpassword } = request.body || {};
+
+        // Sanitize and validate required fields
+        if(!_id) throw new ApiError(400, "State ID is missing");
+        if(!password.trim()) throw new ApiError(400, "Password is required");
+        if(!cpassword.trim()) throw new ApiError(400, "Confirm password is required");
+        if(password !== cpassword) throw new ApiError(400, "Password and confirm password must be identical");
+
+        // Get user
+        const user = await User.findById(_id);
+        if(!user) throw new ApiError(404, "User not found associated with the state ID");
+
+        // Prevent user from reusing the same password
+        const isMatched = await user.matchPassword(password);
+        if(isMatched) throw new ApiError(400, "New password cannot be the same as your current password");
+
+        user.password = password;
+        await user.save(); // Save to trigger password hashing
+        return response.status(200).json(new ApiResponse(200, null, "You have successfully reset your password"));
+    } 
+    catch(error) 
+    {
+        throw error;
+    }
+};
+
+module.exports = { forgotPassword, verifyResetCode, resetPassword };
