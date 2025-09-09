4adb8b425c389e49bcd852ee33b1ae14d21bca1a
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-09 20:27:28 +0500

    Refactor

--- client/src/layout/AuthLayout/index.jsx
+++ client/src/layout/AuthLayout/index.jsx
@@ -3,9 +3,7 @@ import { Outlet } from "react-router-dom";
 function AuthLayout() 
 {
     return (
-        <div className="d-flex justify-content-center align-items-center vh-100">
-            <Outlet />
-        </div>
+        <Outlet />
     );
 }
 
