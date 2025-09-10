dfb56ed078c8dd1f348b81257bb254b038517928
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-10 17:34:06 +0500

    Listen on server

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const connectDB = require("./database/connection");
-const app = require("./app");
 const { port } = require("./constants");
+const { server } = require("./app");
 
 connectDB()
 .then(() => {
-    app.on("error", (error) => console.log(`Express app is failed to listen! ${error}`));
-    app.listen(port, () => console.log(`Server is listening at port ${port}`));
+    server.on("error", (error) => console.log(`Express app is failed to listen! ${error}`));
+    server.listen(port, () => console.log(`Server is listening at port ${port}`));
 })
 .catch(error => console.log(`Database connection failed! ${error.message}`));
