7a311bc5848273d183821674d0005b988db46e06
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-10 15:32:36 +0500

    Registered auth route

--- server/src/app.js
+++ server/src/app.js
@@ -22,19 +22,19 @@ app.use("/public", express.static(path.resolve("public")));
 const userRouter = require("./routes/user");
 const tafseerRouter = require("./routes/tafseer");
 const bookRouter = require("./routes/book");
-// const videoRouter = require("./routes/video");
 const audioRouter = require("./routes/audio");
 const mailRouter = require("./routes/mails");
 const securityRouter = require("./routes/security");
+const authRouter = require("./routes/auth");
 
 // Registered routes
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/tafseer", tafseerRouter);
 app.use("/api/v1/book", bookRouter);
-// app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/audio", audioRouter);
 app.use("/api/v1/mail", mailRouter);
 app.use("/api/v1/security", securityRouter);
+app.use("/api/v1/auth", authRouter);
 
 // Error handling middleware
 app.use(errorHandler);
