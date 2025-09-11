e902c098b8b1638a058a48590515f84418b52919
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-11 13:19:18 +0500

    Refactor generate code function with nano id

--- server/src/utils/generateCode.js
+++ server/src/utils/generateCode.js
@@ -1,10 +1,29 @@
-const shortid = require("shortid");
+const { customAlphabet } = require("nanoid");
+const bcrypt = require("bcrypt");
+const ApiError = require("./ApiError");
 
 // Generate code for account activation and password resets with expiry time
-const generateCode = (expiresInMinutes = 15) => {
-    const code = shortid.generate();
+const generateCode = async (length = 9, expiresInMinutes = 15, isHashed = true) => {
+    let hashedCode = null;
+
+    // Validate expiry time
+    if(expiresInMinutes <= 0) throw new ApiError(500, "Expiry time must be positive");
+
+    // To keep only alpha-numeric values
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; 
+
+    // Generate code
+    const nanoid = customAlphabet(alphabet, length);
+    const code = nanoid();
+
+    // Generate expiry time
     const expiresAt = new Date(Date.now() + 1000 * 60 * expiresInMinutes);
-    return { code, expiresAt };
+
+    // If hashed needed
+    if(isHashed) hashedCode = await bcrypt.hash(code, 10);
+
+    // Return payload
+    return { code, hashedCode: isHashed ? hashedCode : null, expiresAt };
 };
 
 module.exports = generateCode;
