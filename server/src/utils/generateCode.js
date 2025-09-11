a80efe7357a85576a63c12f109e40985c5613cd4
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-11 14:13:48 +0500

    Refactor generate code helper function with crypto module

--- server/src/utils/generateCode.js
+++ server/src/utils/generateCode.js
@@ -1,11 +1,9 @@
-const { customAlphabet } = require("nanoid");
+const crypto = require("crypto");
 const bcrypt = require("bcrypt");
 const ApiError = require("./ApiError");
 
 // Generate code for account activation and password resets with expiry time
-const generateCode = async (length = 9, expiresInMinutes = 15, isHashed = true) => {
-    let hashedCode = null;
-
+const generateCode = async (length = 9, expiresInMinutes = 10, isHashed = true) => {
     // Validate expiry time
     if(expiresInMinutes <= 0) throw new ApiError(500, "Expiry time must be positive");
 
@@ -13,17 +11,21 @@ const generateCode = async (length = 9, expiresInMinutes = 15, isHashed = true) 
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; 
 
     // Generate code
-    const nanoid = customAlphabet(alphabet, length);
-    const code = nanoid();
+    let code = "";
+    const bytes = crypto.randomBytes(length);
+    for (let i = 0; i < length; i++) 
+    {
+        code += alphabet[bytes[i] % alphabet.length];
+    }    
 
     // Generate expiry time
     const expiresAt = new Date(Date.now() + 1000 * 60 * expiresInMinutes);
 
     // If hashed needed
-    if(isHashed) hashedCode = await bcrypt.hash(code, 10);
+    const hashedCode = isHashed ? await bcrypt.hash(code, 10) : null;
 
     // Return payload
-    return { code, hashedCode: isHashed ? hashedCode : null, expiresAt };
+    return { code, hashedCode, expiresAt };
 };
 
 module.exports = generateCode;
