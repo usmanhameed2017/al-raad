4141cb4a7b50fca6830d746bf042ed6c20308123
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-09-17 13:08:05 +0500

    Reconfigure socket connection

--- client/src/service/socket.js
+++ client/src/service/socket.js
@@ -1,7 +1,29 @@
 import { io } from "socket.io-client";
 import { backendURL } from "../constants";
 
+// Configuration
+const socket = io(backendURL, { 
+    withCredentials: true, 
+    autoConnect: false 
+});
+
 // Connect with backend
-const socket = io(backendURL, { withCredentials: true });
+export const connectSocket = () => {
+    // Remove previous listeners
+    socket.removeAllListeners();
+
+    try 
+    {
+        // Destroy previous instance if exist
+        if(socket.connected) socket.disconnect();
+        
+        // Fresh connection
+        if(!socket.connected) socket.connect();        
+    } 
+    catch(error) 
+    {
+        console.log("Failed to connect or disconnect socket error:", error.message);
+    }
+};
 
 export default socket;
