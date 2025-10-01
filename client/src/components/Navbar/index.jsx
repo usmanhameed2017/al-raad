6aa0b1c70c2db29213fb1e8703708e23d95d3e5e
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-10-01 21:24:35 +0500

    Refactor navbar component for logo adjustment

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -12,33 +12,30 @@ function NavbarBS()
 
     return (
         <Navbar variant="dark" expand="lg" fixed="top" className={styles.navbar}>
-            <Container fluid>
-                <Navbar.Brand as={NavLink} to="/" className={styles.logo}> Al-Ra’ad </Navbar.Brand>
+            <Container fluid className={styles.containerFix}>
+                {/* Logo */}
+                <Navbar.Brand as={NavLink} to="/" className={styles.logo}>
+                    <img src="/logo.png" alt="Logo" className={styles.logoImg} />
+                </Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="main-navbar" className={styles.hamburger} />
                 <Navbar.Collapse id="main-navbar">
                     <Nav className={`ms-auto ${styles.NavLinks}`}>
                         <Nav.Link as={NavLink} to="/">Home</Nav.Link>
                         <Nav.Link as={NavLink} to="/about">About</Nav.Link>
                         <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
-                        {
-                            userData && ( <Nav.Link as={NavLink} to="/tafseer">Tafseer</Nav.Link> )
-                        }
+                        {userData && <Nav.Link as={NavLink} to="/tafseer">Tafseer</Nav.Link>}
                         <Nav.Link as={NavLink} to="/books">Books</Nav.Link>
                         <Nav.Link as={NavLink} to="/audios">Audios</Nav.Link>
-                        {/* <Nav.Link as={NavLink} to="/videos">Videos</Nav.Link> */}
-                        {
-                            userData ? 
-                            (
-                                <NavDropdown title={ userData?.name || "Account" } className={styles.userDropdown}>
-                                    <NavDropdown.Item as={NavLink} to="/settings">Settings</NavDropdown.Item>
-                                    <NavDropdown.Item onClick={userLogout}>Logout</NavDropdown.Item>
-                                </NavDropdown>
-                            )
-                            :
-                            (
-                                <Nav.Link as={NavLink} to="/login">Signin</Nav.Link>
-                            )
-                        }
+
+                        {userData ? (
+                            <NavDropdown title={userData?.name || "Account"} className={styles.userDropdown}>
+                                <NavDropdown.Item as={NavLink} to="/settings">Settings</NavDropdown.Item>
+                                <NavDropdown.Item onClick={userLogout}>Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        ) : (
+                            <Nav.Link as={NavLink} to="/login">Signin</Nav.Link>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
