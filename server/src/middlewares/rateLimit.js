b9276a33fe68d6d3c562d8e17dc406b478d2cc93
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-10-06 20:11:19 +0500

    Created a custom and reuseable middleware for rate limiting to prevent DDOS attacks

--- server/src/middlewares/rateLimit.js
+++ server/src/middlewares/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 const ApiError = require('../utils/ApiError');
 
 // Limit the requests
-const limitRequest = ({ message = "Too many requests, please try again later", minutes = 1, maxRequests = 10 }) => {
+const limitRequest = ({ minutes = 1, maxRequests = 10, message = "Too many requests, please try again later" }) => {
     const limiter = rateLimit({
         windowMs: 1000 * 60 * minutes,
         max: maxRequests, // Max requests per IP per window
