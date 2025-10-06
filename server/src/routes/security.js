32b11e6ac49a4dd55af5a56a96443f2768b9ed6a
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-10-06 20:26:21 +0500

    Inject rate limit middleware in auth route

--- server/src/routes/security.js
+++ server/src/routes/security.js
@@ -1,12 +1,14 @@
 const { forgotPassword, verifyResetCode, resetPassword} = require("../controllers/security");
 const csrfProtection = require("../middlewares/csrfToken");
+const limitRequest = require("../middlewares/rateLimit");
 
 // Router instance
 const securityRouter = require("express").Router();
 
 // Forgot password
-securityRouter.route("/forgotPassword").post(csrfProtection, forgotPassword);
-securityRouter.route("/verifyResetCode").patch(csrfProtection, verifyResetCode);
+securityRouter.route("/forgotPassword")
+.post(limitRequest({ maxRequests:1, message:"You can request a new verification code after 60 seconds." }), csrfProtection, forgotPassword);
+securityRouter.route("/verifyResetCode").patch(limitRequest({ maxRequests:5 }), csrfProtection, verifyResetCode);
 securityRouter.route("/resetPassword").patch(csrfProtection, resetPassword);
 
 module.exports = securityRouter;
