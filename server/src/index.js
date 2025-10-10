ba3ebf9ee130c303c7f64345cbc8d635a48d9866
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-10-10 21:31:34 +0500

    Scaling application using clusterization

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,34 @@ require("dotenv").config();
 const connectDB = require("./database/connection");
 const { port } = require("./constants");
 const { server } = require("./app");
+const cluster = require("cluster");
+const os = require("os");
 
-connectDB()
-.then(() => {
-    server.on("error", (error) => console.log(`Express app is failed to listen! ${error}`));
-    server.listen(port, () => console.log(`Server is listening at port ${port}`));
-})
-.catch(error => console.log(`Database connection failed! ${error.message}`));
+// Total CPU cores
+const totalCPUS = os.cpus().length;
+
+// Clusterization
+if (cluster.isPrimary) {
+    console.log(`Primary process ${process.pid} is running`);
+    console.log(`Spawning ${totalCPUS} worker processes...`);
+
+    for (let i = 0; i < totalCPUS; i++) 
+    {
+        cluster.fork();
+    }
+
+    // If worker dies
+    cluster.on("exit", (worker) => {
+        console.log(`Worker ${worker.process.pid} died. Restarting...`);
+        cluster.fork();
+    });
+} 
+else 
+{
+    connectDB()
+    .then(() => {
+        server.on("error", (error) => console.log(`Express app failed to listen! ${error}`));
+        server.listen(port, () => console.log(`Server running on port:${port} | PID: ${process.pid}`));
+    })
+    .catch((error) => console.log(`Database connection failed in worker ${process.pid}: ${error.message}`));
+}
