f17d727177c4bf9d57f35c85879ebbf0aed9635a
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-10-22 19:46:33 +0500

    Update: Attached forwardref to formik form

--- client/src/components/Form/index.jsx
+++ client/src/components/Form/index.jsx
@@ -1,20 +1,36 @@
 import { Formik, Form } from 'formik';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import styles from "./style.module.css";
 
-function FormBS({ children, initialValues, validationSchema, handlerFunction, className })
-{
+const FormBS = forwardRef(({ children, initialValues, validationSchema, handlerFunction,
+    validateOnBlur = true, validateOnChange = true,  className }, ref) => {
+    const formikRef = useRef();
+
+    // Expose Formik submit function to parent
+    useImperativeHandle(ref, () => ({
+        submitForm: () => {
+            if(formikRef.current) formikRef.current.handleSubmit();
+        }
+    }));
+
     return (
         <div className={styles.formContainer}>
-            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handlerFunction} 
-            enableReinitialize={true} validateOnBlur={false} validateOnChange={false}>
+            <Formik
+                innerRef={formikRef}
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={handlerFunction}
+                enableReinitialize={true}
+                validateOnBlur={validateOnBlur}
+                validateOnChange={validateOnChange}>
                 {formikProps => (
-                    <Form className={className ? className : `${styles.form}`}>        
-                        { typeof children === "function" ? children(formikProps) : children }
+                    <Form className={className ? className : `${styles.form}`}>
+                        {typeof children === "function" ? children(formikProps) : children}
                     </Form>
-                )}             
+                )}
             </Formik>
         </div>
     );
-}
+});
 
 export default FormBS;
