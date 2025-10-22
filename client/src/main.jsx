cc5dc4c904909e05fe10345600809d484fcd4f78
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-10-22 18:41:14 +0500

    Fix: Move auth provider to main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from "./context/auth";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
 );
