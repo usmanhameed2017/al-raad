c3860dce31dce0b9819a5595a36c1ae685792e2c
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-10-22 19:16:46 +0500

    Move auth provider to app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,9 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import AuthProvider from "./context/auth";
 
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </AuthProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
