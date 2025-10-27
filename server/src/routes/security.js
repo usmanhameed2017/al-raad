066f1097aa178be1074537ad22931659f3bf5808
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-10-27 15:38:42 +0500

    Fix: update error message

--- server/src/routes/security.js
+++ server/src/routes/security.js
@@ -6,7 +6,7 @@ const securityRouter = require("express").Router();
 
 // Forgot password
 securityRouter.route("/forgotPassword")
-.post(limitRequest({ maxRequests:1, message:"You can request a new verification code after 60 seconds." }), forgotPassword);
+.post(limitRequest({ maxRequests:1, message:"You can request a new verification code before 60 seconds." }), forgotPassword);
 securityRouter.route("/verifyResetCode").patch(limitRequest({ maxRequests:5 }), verifyResetCode);
 securityRouter.route("/resetPassword").patch(resetPassword);
 
