4c1ab4dfbd8bf9910fdd379f391669c93d0b2d3c
Usman Hameed <usmanhameed1790+backup@gmail.com>
2025-10-27 12:57:00 +0500

    REmoved extra resource file

--- server/src/middlewares/csrfToken.js
+++
@@ -1,7 +0,0 @@
-const csrf = require("csurf");
-const { cookieOptions } = require("../config");
-
-// CSRF Protection Middleware for unintended form submission
-const csrfProtection = csrf({ cookie: cookieOptions });
-
-module.exports = csrfProtection;
