9d79cc562c11a67dd340f8ba0e89ded8cedca5b9
usmanhameed2017 <usmanhameed1790+backup@gmail.com>
2026-01-01 22:45:15 +0500

    Attach links

--- client/src/components/Hero/index.jsx
+++ client/src/components/Hero/index.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import styles from "./style.module.css";
 import Button from "../Button";
 import { FaBookOpen, FaBookReader, FaPlayCircle } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 function Hero({ type, src, heading, paragrapgh, enableButton, buttonText }) 
 {
@@ -58,29 +59,36 @@ function Hero({ type, src, heading, paragrapgh, enableButton, buttonText })
                     animate={{ opacity: 1, x: 0 }}
                     transition={{ delay: 0.8, duration: 1 }}
                     >
-                    <div className={`${styles.floatingCard} ${styles.cardAyah}`}>
-                        <h3> <FaBookOpen size={30} /> Daily Ayah</h3>
-                        <p>
-                        Indeed, this Qur'an guides to that which is most suitable and
-                        gives good tidings to believers.
-                        </p>
-                    </div>
 
-                    <div className={`${styles.floatingCard} ${styles.cardBooks}`}>
-                        <h3> <FaBookReader size={30} /> Islamic Books</h3>
-                        <p>
-                        Explore authentic books of Hadith, Fiqh, Tafseer and Islamic
-                        studies — all at one place.
-                        </p>
-                    </div>
+                    <Link to={`/tafseer`}>
+                        <div className={`${styles.floatingCard} ${styles.cardAyah}`}>
+                            <h3> <FaBookOpen size={30} /> Daily Ayah</h3>
+                            <p>
+                            Indeed, this Qur'an guides to that which is most suitable and
+                            gives good tidings to believers.
+                            </p>
+                        </div>
+                    </Link>
 
-                    <div className={`${styles.floatingCard} ${styles.cardAudio}`}>
-                        <h3> <FaPlayCircle size={30} /> Audio Lectures</h3>
-                        <p>
-                        Listen to lectures, sermons, and Quran recitations to enrich your
-                        spiritual journey.
-                        </p>
-                    </div>
+                    <Link to={`/books`}>
+                        <div className={`${styles.floatingCard} ${styles.cardBooks}`}>
+                            <h3> <FaBookReader size={30} /> Islamic Books</h3>
+                            <p>
+                            Explore authentic books of Hadith, Fiqh, Tafseer and Islamic
+                            studies — all at one place.
+                            </p>
+                        </div>                       
+                    </Link>
+
+                    <Link to={`/audios`}>
+                        <div className={`${styles.floatingCard} ${styles.cardAudio}`}>
+                            <h3> <FaPlayCircle size={30} /> Audio Lectures</h3>
+                            <p>
+                            Listen to lectures, sermons, and Quran recitations to enrich your
+                            spiritual journey.
+                            </p>
+                        </div>                        
+                    </Link>
                     </motion.div>
                 </div>
             </section>            
